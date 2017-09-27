0f2b7f14fed04a34a8b21e4199579f198bbaa30f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-09-27 16:10:07 -0400

    Adding the template version of README.md, gatorgrouper, and requirements.txt.

---
+++ README.md
@@ -0,0 +1,142 @@
+# GatorGrader
+
+Designed for use with [GitHub Classroom](https://classroom.github.com/),
+GatorGrader is an automated grading tool that checks the work of programmers and
+writers. Although it can be used independently of a containing project,
+GatorGrader is commonly used by faculty in the [Department of Computer Science
+at Allegheny College](https://github.com/Allegheny-Computer-Science) as a Git
+submodule in a Git repository for a specific assignment. Since it is a Python 3
+program that can easily be run on the command-line, GatorGrader also works
+nicely with [Travis CI](https://travis-ci.com/) &mdash; which is again how
+faculty members at Allegheny College commonly use it in conjunction with other
+automated checking tools.
+
+## Installing GatorGrader
+
+As a Python 3 program, GatorGrader relies on
+[pip](https://pip.pypa.io/en/stable/installing/) for installation. To ensure
+that all of the dependencies are installed correctly, please type
+the following commands before running GatorGrader.
+
+- `pip install --upgrade pip`
+- `pip install -r requirements.txt`
+
+Note that you may have Python 3 setup in different ways on your computer. For
+instance, you may prefer to install GatorGrader's dependencies in a site-wide
+location and then you would have to type, for instance, `sudo pip install -r
+requirements.txt`. Alternatively, you may choose to install the dependencies by
+typing `pip install --user -r requirements.txt`.
+
+GatorGrader was developed to easily run in conjunction with a [venv-based Python
+3 virtual environment](https://docs.python.org/3/library/venv.html). This means
+that if you are in the directory that contains the `gatorgrader` directory then
+you could type `python3 -m venv gatorgrader` to create all of the components of
+a venv-based virtual environment in the `gatorgrader` directory. Once you
+complete this step, you can type the command `source gatorgrader/bin/activate`
+to activate the venv-based virtual environment. Interested in learning more
+about the basics of virtual environments in Python 3? You can read this
+[article](http://www.cs.allegheny.edu/sites/gkapfham/programming/research/idea/2017/07/14/Virtual-Environments/)
+to further develop your understanding of this topic.
+
+## Testing GatorGrader
+
+GatorGrader uses [pytest](https://docs.pytest.org/en/latest/) for testing. To
+run GatorGrader's comprehensive test suite of over 150 test cases, please type
+`pytest tests` in the root of the project's repository. If any of the test cases
+fail in your development environment, please please raise an issue in
+GatorGrader's issue tracker.
+
+## Running GatorGrader
+
+If your environment supports it, then please set the `GATORGRADER_HOME`
+environment variable. For instance, typing the command `export
+GATORGRADER_HOME="/home/travis/build/gkapfham/gatorgrader"` would set
+`GATORGRADER__HOME` environment variable to the appropriate directory for
+building it on Travis CI under the `gkapfham` account. If you do not set the
+`GATORGRADER__HOME` environment variable, then GatorGrader will attempt to guess
+the best setting for it.
+
+GatorGrader can perform simple checks on both writing and source code. For
+instance, the following command uses GatorGrader to ensure that the
+`internal/java` directory contains the file called `Sample.java` and that
+this file contains at least two single-line comments (e.g., those lines that
+start with `//`) and two multiple-line comments (e.g., content that is
+surrounded by `/** */`).
+
+```
+python3 gatorgrader.py \
+        --directories internal/java \
+        --checkfiles Sample.java \
+        --singlecomments 2 \
+        --multicomments 2 \
+        --languages Java
+```
+
+Since introductory courses at Allegheny College commonly use the Java
+programming language and compile and run programs using
+[Gradle](https://gradle.org/), the following commands shows how to use
+GatorGrader to ensure that running the program yields exactly four lines of
+output.
+
+```
+python3 gatorgrader/gatorgrader.py \
+        --commands "gradle run" \
+        --outputlines 4 \
+        --languages Java
+```
+
+Since many computer science courses at Allegheny College require students to
+write technical documents, GatorGrader also provides a feature to check how many
+paragraphs of writing are in a file. The following example shows how to use
+GatorGrader to ensure that the `README.md` file in the root of this repository
+contains at least four paragraphs of writing.
+
+```
+python3 gatorgrader.py \
+        --directories . \
+        --checkfiles README.md \
+        --paragraphs 4
+```
+
+Each of the previous commands were run on an Ubuntu 16.04 workstation running
+Python 3.5.2. However, GatorGrader should run correctly on a wide variety of
+operating systems that support Python version 3.
+
+## GatorGrader in Action
+
+GatorGrader is commonly used in conjunction with other tools that check source
+code. For instance, in the introductory computer science classes at Allegheny
+College, the submissions are verified by
+[Checkstyle](https://github.com/checkstyle/checkstyle) and thus the Java source
+code must adhere to all of the requirements in the [Google Java Style
+Guide](https://google.github.io/styleguide/javaguide.html). Moreover, Markdown
+files that contain writing must meet the standards described in the [Markdown
+Syntax Guide](https://guides.github.com/features/mastering-markdown/), meaning
+that all Markdown files must pass the checks performed by the [Markdown linting
+tool](https://github.com/markdownlint/markdownlint). Finally, all submitted
+technical writing must adhere to the writing standards set by the [Proselint
+tool](http://proselint.com/).
+
+The solution repositories for the laboratory assignments in Computer Science
+courses at Allegheny College are kept private. However, the "starter"
+repositories for assignments are publicly available so as to support their
+integration into [GitHub Classroom](https://classroom.github.com/). As
+GatorGrader continues to be adopted by more courses, we will expand this list of
+GitHub repositories that provide starting code templates and furnish GatorGrader
+as a Git submodule.
+
+- [Computer Science 111 Laboratory #1](https://github.com/Allegheny-Computer-Science-111-F2017/cs111-F2017-lab1-starter)
+
+## Acknowledgements
+
+We gratefully acknowledge Nicholas Tocci and Race Mahoney for pointing out that
+the GatorGrader submodule did not have a `.gitignore` file, thus causing the
+derived Python files and directories to seem to be untracked.
+
+## Problems or Praise
+
+If you have any problems with installing or using GatorGrader, then please
+create an issue associated with this Git repository using the "Issues" link at
+the top of this site. The contributors to GatorGrader will do all that they can
+to resolve your issue and ensure that the entire tool works well in your
+teaching and development environment.
---
+++ gatorgrouper.py
@@ -0,0 +1,186 @@
+""" GatorGrader checks the files of programmers and writers """
+
+import os
+import sys
+
+import argparse
+
+import gatorgrader_invoke
+import gatorgrader_exit
+
+SLASH = "/"
+GATORGRADER_HOME = "GATORGRADER_HOME"
+
+DEFAULT_COUNT = 0
+DEFAULT_LANGUAGE = "Java"
+INCORRECT_ARGUMENTS = 2
+JAVA = "Java"
+PYTHON = "Python"
+
+SINGLE = "single-line"
+MULTIPLE = "multiple-line"
+
+
+def parse_gatorgrader_arguments(args):
+    """ Parses the arguments provided on the command-line """
+    gg_parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    gg_parser.add_argument(
+        "--nowelcome",
+        help="Do not display the welcome message",
+        action="store_true")
+
+    gg_parser.add_argument('--checkfiles', nargs='+', type=str)
+    gg_parser.add_argument('--directories', nargs='+', type=str)
+
+    gg_parser.add_argument('--singlecomments', nargs='+', type=int)
+
+    gg_parser.add_argument('--multicomments', nargs='+', type=int)
+
+    gg_parser.add_argument('--paragraphs', nargs='+', type=int)
+
+    gg_parser.add_argument('--fragments', nargs='+', type=str)
+    gg_parser.add_argument('--fragmentcounts', nargs='+', type=int)
+
+    gg_parser.add_argument('--languages', nargs="+", type=str)
+
+    gg_parser.add_argument('--commands', nargs='+', type=str)
+    gg_parser.add_argument('--outputlines', nargs='+', type=int)
+
+    gg_arguments_finished = gg_parser.parse_args(args)
+    return gg_arguments_finished
+
+
+def verify_gatorgrader_arguments(args):
+    """ Checks if the arguments are correct """
+    verified_arguments = True
+    # TODO: This verification is not complete and/or incorrect
+    if args.checkfiles is not None and args.directories is None:
+        verified_arguments = False
+    elif args.directories is not None and args.checkfiles is None:
+        verified_arguments = False
+    elif args.directories is not None and args.checkfiles is not None:
+        if len(args.directories) != len(args.checkfiles):
+            verified_arguments = False
+    elif args.singlecomments is not None:
+        if args.checkfiles is None or args.directories is None:
+            verified_arguments = False
+    elif args.multicomments is not None:
+        if args.checkfiles is None or args.directories is None:
+            verified_arguments = False
+    elif args.paragraphs is not None:
+        if args.checkfiles is None or args.directories is None:
+            verified_arguments = False
+    # elif args.fragments is not None or args.fragmentcounts is not None:
+        # if args.checkfiles is None or args.directories is None or args.commands is None:
+            # verified_arguments = False
+    elif args.fragments is None and args.fragmentcounts is not None:
+        verified_arguments = False
+    # elif args.fragments is not None and (args.fragmentcounts is None and args.commands is None):
+        # verified_arguments = False
+    return verified_arguments
+
+
+def verify_gatorgrader_home(current_gatorgrader_home):
+    """ Verifies that the GATORGRADER_HOME environment variable is set correctly """
+    verified_gatorgrader_home = False
+    if current_gatorgrader_home is not None and current_gatorgrader_home.endswith(
+            SLASH) is True:
+        verified_gatorgrader_home = True
+    return verified_gatorgrader_home
+
+
+def get_gatorgrader_home():
+    """ Returns the GATORGRADER_HOME """
+    current_gatorgrader_home = os.environ.get(GATORGRADER_HOME)
+    had_to_set = False
+    # the current gatorgrader_home is acceptable, so use it
+    if verify_gatorgrader_home(current_gatorgrader_home) is not False:
+        gatorgrader_home = current_gatorgrader_home
+    # the current gatorgrader_home is not okay, so guess at one
+    else:
+        gatorgrader_home = os.getcwd() + SLASH
+        had_to_set = True
+    return gatorgrader_home, had_to_set
+
+
+def display_welcome_message():
+    """ Display a welcome message """
+    print()
+    print(
+        "GatorGrader: Automatically Check the Files of Programmers and Writers"
+    )
+    print("https://github.com/gkapfham/gatorgrader")
+    print()
+
+
+def display_checking_message():
+    """ Display the checking message """
+    print("Valid command-line arguments.")
+    print("Running the specified checks!")
+    print()
+
+
+if __name__ == '__main__':
+    # parse and verify the arguments
+    gg_arguments = parse_gatorgrader_arguments(sys.argv[1:])
+    did_verify_arguments = verify_gatorgrader_arguments(gg_arguments)
+    # incorrect arguments, exit program
+    if did_verify_arguments is False:
+        print("Incorrect command-line arguments.")
+        sys.exit(INCORRECT_ARGUMENTS)
+    # correct arguments, so perform the checks
+    else:
+        if gg_arguments.nowelcome is not True:
+            display_welcome_message()
+            display_checking_message()
+        check_return_values = []
+        # CHECK: all of the files exist in their directories
+        if gg_arguments.directories is not None and gg_arguments.checkfiles is not None:
+            current_invoke_return_values = gatorgrader_invoke.invoke_all_file_in_directory_checks(
+                gg_arguments.checkfiles, gg_arguments.directories)
+            check_return_values.extend(current_invoke_return_values)
+            # CHECK: Java code contains 'k' single-line comments
+            if gg_arguments.singlecomments is not None:
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_comment_checks(
+                    gg_arguments.checkfiles, gg_arguments.directories,
+                    gg_arguments.singlecomments, SINGLE,
+                    gg_arguments.languages)
+                check_return_values.extend(current_invoke_return_values)
+            # CHECK: Java code contains 'k' multiple-line comments
+            if gg_arguments.multicomments is not None:
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_comment_checks(
+                    gg_arguments.checkfiles, gg_arguments.directories,
+                    gg_arguments.multicomments, MULTIPLE,
+                    gg_arguments.languages)
+                check_return_values.extend(current_invoke_return_values)
+            # CHECK: Writing contains 'k' paragraphs
+            if gg_arguments.paragraphs is not None:
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_paragraph_checks(
+                    gg_arguments.checkfiles, gg_arguments.directories,
+                    gg_arguments.paragraphs)
+                check_return_values.extend(current_invoke_return_values)
+            # CHECK: Content contains 'k' specified fragments
+            if gg_arguments.fragments is not None and gg_arguments.fragmentcounts is not None:
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_fragment_checks(
+                    gg_arguments.checkfiles, gg_arguments.directories,
+                    gg_arguments.fragments, gg_arguments.fragmentcounts)
+                check_return_values.extend(current_invoke_return_values)
+        # CHECK: Command produces 'k' lines of output
+        elif gg_arguments.commands is not None and gg_arguments.outputlines is not None:
+            current_invoke_return_values = gatorgrader_invoke.invoke_all_command_checks(
+                gg_arguments.commands, gg_arguments.outputlines,
+                gg_arguments.languages)
+        # CHECK: Command produces lines of output with the specified fragment
+        elif gg_arguments.commands is not None and gg_arguments.fragments is not None:
+            current_invoke_return_values = gatorgrader_invoke.invoke_all_command_fragment_checks(
+                gg_arguments.commands, gg_arguments.fragments,
+                gg_arguments.languages)
+        # The requested check is not available
+        else:
+            print("Requested non-existent checking.")
+
+        # DONE: Determine the correct exit code for the checks
+        correct_exit_code = gatorgrader_exit.get_code(check_return_values)
+        sys.exit(correct_exit_code)
