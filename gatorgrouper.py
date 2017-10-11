49d930f06aa79b4dca4f414a40be6f57c9b4aacd
yeeunmariakim <mariagracekim@gmail.com>
2017-10-11 00:04:32 -0400

    fixing calls to gatorgrader function

--- gatorgrouper.py
+++ gatorgrouper.py
@@ -5,7 +5,7 @@ import argparse
 import itertools
 import sys
 #function files
-from parse_gatorgrader_arguments import parse_gatorgrader_arguments
+from parse_gatorgrouper_arguments import parse_gatorgrouper_arguments
 from read_student_file import read_student_file
 from display_diagnostics import display_diagnostics
 from display_student_identifiers import display_student_identifiers
@@ -17,22 +17,22 @@ from display_welcome_message import display_welcome_message
 
 if __name__ == '__main__':
     # parse the arguments and display welcome message
-    gg_arguments = parse_gatorgrader_arguments(sys.argv[1:])
+    gg_arguments = parse_gatorgrouper_arguments(sys.argv[1:])
     display_welcome_message()
-    if gg_arguments.verbose is True:
+    if gg_arguments.debug is True:
         print("Configuration of GatorGrouper:")
         print(gg_arguments)
         print()
     # read in the student identifiers from the specified file
     student_identifers = read_student_file(gg_arguments.students_file)
-    if gg_arguments.verbose is True:
+    if gg_arguments.debug is True:
         print("GatorGrouper will group these students:")
         print()
         display_student_identifiers(student_identifers)
         print()
     # shuffle the student identifiers
     shuffled_student_identifers = shuffle_students(student_identifers)
-    if gg_arguments.verbose is True:
+    if gg_arguments.debug is True:
         print("GatorGrouper randomly ordered the students:")
         print()
         display_student_identifiers(shuffled_student_identifers)
@@ -43,4 +43,3 @@ if __name__ == '__main__':
     display_diagnostics(shuffled_student_identifers,
                         grouped_student_identifiers)
     display_student_groups(grouped_student_identifiers)
-    
