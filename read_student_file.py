3f77619c61f91181b8ff2bc8372c271923f18986
Zachary Shaffer <shafferz@allegheny.edu>
2017-10-12 19:55:40 -0400

    Update read_student_file.py

--- read_student_file.py
+++ read_student_file.py
@@ -5,11 +5,17 @@ import argparse
 import itertools
 import sys
 
+size = 0
 
 def read_student_file(students_file_name):
     """ Reads the student identifies from the specific file,
         returning the identifiers in a list """
     with open(students_file_name, 'r') as students_file:
         student_identifers = [line.strip() for line in students_file]
+    size = len(student_identifers)
     return student_identifers
 
+def student_list_length():
+    """ Returns the number of students in the 
+        list of student_identifiers """
+    return size
