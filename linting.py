b1360e374ab10f4454c2aec12305e939fe322d34
Tyler Lyle <lylet@aldenv162.allegheny.edu>
2017-10-12 14:15:12 -0400

    updated testcase

--- linting.py
+++ linting.py
@@ -5,7 +5,13 @@ Requires
 """
 
 import pytest
+import glob
 from flake8.api import legacy as flake8
 
 def test_1():
     assert True
+
+    filenames = glob.glob('gatorgrouper/*.py')
+    style_guide = flake8.get_style_guide(ignore=['E24', 'W503'])
+    report = style_guide.check_files(filenames)
+    assert report.get_statistics('E') == [], 'Flake8 found violations'
