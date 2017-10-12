8338af25a7c619175f2c7c2327893460db698b26
bristola <bristola@allegheny.edu>
2017-10-12 13:59:41 -0400

    Added import statements to test installation

---
+++ linting.py
@@ -0,0 +1,11 @@
+"""
+run with `pytest linting.py`
+This will get moved to the test suite when it is completed.
+Requires
+"""
+
+import pytest
+from flake8.api import legacy as flake8
+
+def test_1():
+    assert True
