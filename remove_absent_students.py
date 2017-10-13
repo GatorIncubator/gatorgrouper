6cb6d3a732bd8a037edd095b003fef9e02469beb
Travis Thomas <thomast2@aldenv155.allegheny.edu>
2017-10-12 20:18:43 -0400

    improve syntax and pylint score

--- remove_absent_students.py
+++ remove_absent_students.py
@@ -1,13 +1,15 @@
-from random import shuffle
-import argparse
-import itertools
-import sys
+"""Remove missing students from group assignment"""
+#from random import shuffle
+#import itertools
+#import argparse
+#import sys
 #function files
 
 def remove_absent_students(absentee_list, list_of_student_lists):
+    """Remove missing students before group assignment"""
     list_of_student_lists_copy = list_of_student_lists[:]
     for name in absentee_list:
-        for student_list in list_of_student_lists :
-            if student_list[0] == name :
+        for student_list in list_of_student_lists:
+            if student_list[0] == name:
                 list_of_student_lists_copy.remove(student_list)
     return list_of_student_lists_copy
