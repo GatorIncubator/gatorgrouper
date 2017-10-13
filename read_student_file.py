892641f8ebd7f6d43840a5bd7c28545c3b190c1c
castellanosr <31486151+castellanosr@users.noreply.github.com>
2017-10-13 16:17:50 -0400

    Reverted to how this file looke in Master
    The changes made last night were not completely removed.

--- read_student_file.py
+++ read_student_file.py
@@ -1,21 +1,13 @@
-""" GatorGrouper randomly assigns a list of students to groups """
+""" Reads the student identifies from the specific file,
+        returning the identifiers in a list """
 
 from random import shuffle
 import argparse
 import itertools
 import sys
 
-size = 0
 
 def read_student_file(students_file_name):
-    """ Reads the student identifies from the specific file,
-        returning the identifiers in a list """
     with open(students_file_name, 'r') as students_file:
         student_identifers = [line.strip() for line in students_file]
-    size = len(student_identifers)
     return student_identifers
-
-def student_list_length():
-    """ Returns the number of students in the
-        list of student_identifiers """
-    return size
