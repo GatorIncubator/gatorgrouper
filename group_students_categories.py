7fc2af8f1e322f4ec784de0ff5ef204ae2bb693d
Zachary Andrews <andrewsz@allegheny.edu>
2017-10-13 14:27:35 -0400

    continued to improve grouping based on categories

--- group_students_categories.py
+++ group_students_categories.py
@@ -4,12 +4,14 @@ import itertools
 import sys
 #default values
 from defaults import *
+from shuffle_students import shuffle_students
 
 def group_students_categories(student_identifers, group_size):
 	students_categories = []
 	for student in student_identifers:
 		students_categories.append(student.split(',')[1:])
-	print(students_categories)
+	#print(students_categories)
+	
 	iterable = iter(student_identifers)
 	student_groups = list(
 		iter(lambda: list(itertools.islice(iterable, group_size)), []))
@@ -19,18 +21,21 @@ def group_students_categories(student_identifers, group_size):
 		too_small_group = student_groups[last_group_index]
 		receiving_group.append(*too_small_group)
 		student_groups.remove(too_small_group)
+		
 	good = 0
 	for group in student_groups:
 		score = 0
 		for student in group:
 			score += int(student.split(',')[1])
-		if(score == group_size):
+		print(score)
+		print()
+		if(score == 0):
+			group_students_categories(shuffle_students(student_identifers), group_size)
+		if(score >= group_size/2):
 			good += 1
-		#if(score == 0):
-			#group_students_categories(student_identifers, group_size)
-		#print(str(score))
-	print(str(good))
-	print(str((group_size * .5)))
-	if(good < (group_size * .5)):
-		group_students_categories(student_identifers, group_size)
+	print(good)
+	print(student_groups)
+	print()
+	if(good < (len(student_groups) * .5)):
+		group_students_categories(shuffle_students(student_identifers), group_size)
 	return student_groups
