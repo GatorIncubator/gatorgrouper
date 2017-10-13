5f5fc6974d3739bef91c6ca55090fbfe0bb46f45
Jenna Millin <jennamillin@Jennas-MacBook-Pro.local>
2017-10-12 20:07:24 -0400

    wrote the function to remove students from the list of lists

--- remove_absent_students.py
+++ remove_absent_students.py
@@ -3,16 +3,11 @@ import argparse
 import itertools
 import sys
 #function files
-from parse_gatorgrader_arguments import parse_gatorgrader_arguments
-from read_student_file import read_student_file
-from display_diagnostics import display_diagnostics
-from display_student_identifiers import display_student_identifiers
-from display_student_groups import display_student_groups
-from shuffle_students import shuffle_students
-from group_students import group_students
-from display_welcome_message import display_welcome_message
-from gatorgrouper import gatorgrouper
 
-def remove_absent_students():
-    absent = rawinput("Are any students absent? yes or no")
-    if absent = 'yes':
+def remove_absent_students(absentee_list, list_of_student_lists):
+    list_of_student_lists_copy = list_of_student_lists[:]
+    for name in absentee_list:
+        for student_list in list_of_student_lists :
+            if student_list[0] == name :
+                list_of_student_lists_copy.remove(student_list)
+    return list_of_student_lists_copy
