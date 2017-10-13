a9817e977a97d7ef8d64d7ae57599eda2b0061c3
castellanosr <31486151+castellanosr@users.noreply.github.com>
2017-10-13 14:50:48 -0400

    Added missing "i"s

--- display_diagnostics.py
+++ display_diagnostics.py
@@ -6,9 +6,9 @@ import itertools
 import sys
 
 
-def display_diagnostics(student_identifers, student_groups):
+def display_diagnostics(student_identifiers, student_groups):
     print("Successfully placed",
-          len(student_identifers), "students into",
+          len(student_identifiers), "students into",
           len(student_groups), "groups")
     print()
 
