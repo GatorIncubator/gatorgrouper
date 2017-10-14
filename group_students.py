e72b5907475c5063b804bdb6702565272e43c424
yeeunmariakim <yeeunmariakim@gmail.com>
2017-10-13 21:26:08 -0400

    adding logging for removing a group with only one member and placing the member in the group before it

--- group_students.py
+++ group_students.py
@@ -4,9 +4,9 @@ from random import shuffle
 import argparse
 import itertools
 import sys
+import logging
 #default values
 from defaults import *
-
 
 def group_students(student_identifers, group_size):
     """ Group the student identifiers """
@@ -17,9 +17,9 @@ def group_students(student_identifers, group_size):
     # merge a single student into the previous group
     last_group_index = len(student_groups) - 1
     if len(student_groups[last_group_index]) == SINGLETON_GROUP:
+        logging.info("Removing last group with only one member and placing member into previous group.")
         receiving_group = student_groups[last_group_index - 1]
         too_small_group = student_groups[last_group_index]
         receiving_group.append(*too_small_group)
         student_groups.remove(too_small_group)
     return student_groups
-
