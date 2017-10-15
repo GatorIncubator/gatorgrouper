090745adb0708a6fa1b9d601e0179fbbecf67b96
jerfenson <cerdamejiaj@allegheny.edu>
2017-10-15 19:13:10 -0400

    adding the test cases for parse_gatorgrouper_arguments, display_diagnostics. For any changes just comment on this commit.

---
+++ tests/JC_Test.py
@@ -0,0 +1,34 @@
+def test_parse_gatorgrouper_arguments1(args):
+    args = []
+    parsed_args = parse_gatorgrouper_arguments(args)
+    assert parsed_args.logging_level == logging.ERROR
+    assert parsed_args.group_size == defaults.DEFAULT_TEAM_SIZE
+    assert parsed_args.students_file == defaults.DEFAULT_STUDENT_FILE
+    assert parsed_args.grouping_method == grouping_method.RANDOM
+    assert parsed_args.absentees == No
+
+def test_parse_gatorgrouper_arguments2(args):
+    args = ['--debug', '--students-file', 'students.csv', '--random']
+    parsed_args = parse_gatorgrouper_arguments(args)
+    assert parsed_args.logging_level == logging.DEBUG
+    assert parsed_args.students_file == 'students.csv'
+    assert parsed_args.grouping_method == grouping_method.RANDOM
+
+def test_parse_gatorgrouper_arguments3(args):
+    args = ['--verbose', '--sudoku']
+    parsed_args = parse_gatorgrouper_arguments(args)
+    assert parsed_args.logging_level == logging.INFO
+    assert parsed_args.grouping_method == grouping_method.SUDOKU
+
+def test_parse_gatorgrouper_arguments4(args):
+    args = ['--absentees', 'maria', '--round-robin', '--group-size', '3']
+    parsed_args = parse_gatorgrouper_arguments(args)
+    assert parsed_args.group_size == 3
+    assert parsed_args.grouping_method == grouping_method.ROUND_ROBIN
+    assert parsed_args.absentees == ['maria']
+
+def test_display_diagnostics(student_identifers, student_groups):
+    student_identifers = len(student_identifers)
+    student_groups = len(student_groups)
+    assert  student_identifers == len(student_identifers)
+    assert  student_groups == len(student_groups)
--- tests/Test_Parse_gatorgrouper_argument.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import logging
-import grouping_method
-import defaults
-import parse_gatorgrouper_arguments
-
-
-Test_check_valid_group_size(group_size, students_list)
-## tests the group sizes
