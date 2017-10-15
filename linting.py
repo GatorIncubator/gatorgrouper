39551be1085c0a63786445e66eb10317483baff0
bristola <bristola@allegheny.edu>
2017-10-15 15:54:33 -0400

    Added functionality to check py files in subdirectories of gatorgrouper

--- linting.py
+++ linting.py
@@ -7,9 +7,18 @@ Requires
 import pytest
 import glob
 from flake8.api import legacy as flake8
+import os
 
 def test_flake8():
-    filenames = glob.glob('gatorgrouper/*.py')
+
+    # list of all file names to be checked for PEP8
+    filenames = list()
+
+    # fill list with all python files found in all subdirectories
+    for root, dirs, files in os.walk("gatorgrouper", topdown=False):
+        pyFiles = glob.glob(root+"/*.py")
+        filenames.extend(pyFiles)
+
     style_guide = flake8.get_style_guide(ignore=['E24', 'W503'])
     report = style_guide.check_files(filenames)
     assert report.get_statistics('E') == [], 'Flake8 found violations'
