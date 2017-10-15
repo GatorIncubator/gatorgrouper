dd2d8c09d1147004de6fe7f1091537ee37aee05d
jerfenson <cerdamejiaj@allegheny.edu>
2017-10-15 13:15:37 -0400

    uploading the tests folder which will have the test suite

---
+++ tests/Test_Parse_gatorgrouper_argument.py
@@ -0,0 +1,9 @@
+import pytest
+import logging
+import grouping_method
+import defaults
+import parse_gatorgrouper_arguments
+
+
+Test_check_valid_group_size(group_size, students_list)
+## tests the group sizes
