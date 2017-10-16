a991e77b83be91a3c49949ffb2640c1038be3c62
regent21 <etoccin21@gmail.com>
2017-10-16 15:50:32 -0400

    updated my to do list

--- tests/NTtests.py
+++ tests/NTtests.py
@@ -1,9 +1,14 @@
 """Test suite for the gatorgrouper.py module"""
+#To do:
+#check_valid_group_size
+#display_student_groups
+#remove_absent_students
+#round robin
+#score group
 import pytest
 import parse_gatorgrouper_arguments
 import display_student_groups
 import remove_absent_students
-import sys
 
 def test_check_valid_group_size_one():
     """Checking the group size of one"""
@@ -16,21 +21,21 @@ def test_check_valid_group_size_quarter():
     """Checking the group size of one"""
     student_identifiers = ['maria', 'dan', 'simon', 'jesse', 'jon', 'michael', 'jacob', 'kapfhammer']
     group_size = len(student_identifiers)/4
-    student_groups = parse_gatorgrouper_arguments.check_valid_group_size(group_size,student_identifiers)
+    student_groups = parse_gatorgrouper_arguments.check_valid_group_size(group_size, student_identifiers)
     assert student_groups == True
 
 def test_check_valid_group_size_half():
     """Checking the group size of half of the list length"""
     student_identifiers = ['maria', 'dan', 'simon', 'jesse', 'jon', 'michael', 'jacob', 'kapfhammer']
     group_size = len(student_identifiers)/2
-    student_groups = parse_gatorgrouper_arguments.check_valid_group_size(group_size,student_identifiers)
+    student_groups = parse_gatorgrouper_arguments.check_valid_group_size(group_size, student_identifiers)
     assert student_groups == True
 
 def test_check_valid_group_size_over():
     """Checking the group size of over the list length"""
     student_identifiers = ['maria', 'dan', 'simon', 'jesse', 'jon', 'michael', 'jacob', 'kapfhammer']
     group_size = 9
-    student_groups = parse_gatorgrouper_arguments.check_valid_group_size(group_size,student_identifiers)
+    student_groups = parse_gatorgrouper_arguments.check_valid_group_size(group_size, student_identifiers)
     assert student_groups == False
 
 def test_display_student_groups(capsys):
@@ -43,15 +48,15 @@ def test_display_student_groups(capsys):
 def test_remove_absent_students_one():
     """Checking to see if absent one student is removed"""
     absent_list = ['Nick']
-    list_of_student_of_lists = [['Nick', 0, 1,0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
+    list_of_student_of_lists = [['Nick', 0, 1, 0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
     removed_list = remove_absent_students.remove_absent_students(absent_list, list_of_student_of_lists)
     assert (absent_list in removed_list) is False
     assert len(removed_list) == 2
 
 def test_remove_absent_students_two():
-    """Checking to see if absent one student is removed"""
+    """Checking to see if absent two students is removed"""
     absent_list = ['Nick', 'Marvin']
-    list_of_student_of_lists = [['Nick', 0, 1,0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
+    list_of_student_of_lists = [['Nick', 0, 1, 0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
     removed_list = remove_absent_students.remove_absent_students(absent_list, list_of_student_of_lists)
     assert (absent_list in removed_list) is False
     assert len(removed_list) == 1
