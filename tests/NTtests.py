b92f06c2ce2c080b6dac179ebc71b31ae9a38bd9
regent21 <etoccin21@gmail.com>
2017-10-16 22:19:09 -0400

    completing the last of the test cases

--- tests/NTtests.py
+++ tests/NTtests.py
@@ -1,14 +1,9 @@
 """Test suite for the gatorgrouper.py module"""
-#To do:
-#check_valid_group_size
-#display_student_groups
-#remove_absent_students
-#round robin
-#score group
 import pytest
 import parse_gatorgrouper_arguments
 import display_student_groups
 import remove_absent_students
+import group_random
 
 def test_check_valid_group_size_one():
     """Checking the group size of one"""
@@ -49,14 +44,43 @@ def test_remove_absent_students_one():
     """Checking to see if absent one student is removed"""
     absent_list = ['Nick']
     list_of_student_of_lists = [['Nick', 0, 1, 0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
+    correct = [['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
     removed_list = remove_absent_students.remove_absent_students(absent_list, list_of_student_of_lists)
     assert (absent_list in removed_list) is False
     assert len(removed_list) == 2
+    assert correct == removed_list
 
 def test_remove_absent_students_two():
     """Checking to see if absent two students is removed"""
     absent_list = ['Nick', 'Marvin']
     list_of_student_of_lists = [['Nick', 0, 1, 0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
+    correct = [["Evin", 1, 1, 0]]
     removed_list = remove_absent_students.remove_absent_students(absent_list, list_of_student_of_lists)
     assert (absent_list in removed_list) is False
     assert len(removed_list) == 1
+    assert correct == removed_list
+
+def test_remove_absent_students_all():
+    """Checking to see if absent all students are removed"""
+    absent_list = ['Nick', 'Marvin', 'Evin']
+    list_of_student_of_lists = [['Nick', 0, 1, 0], ['Marvin', 0, 1, 1], ["Evin", 1, 1, 0]]
+    correct = []
+    removed_list = remove_absent_students.remove_absent_students(absent_list, list_of_student_of_lists)
+    assert (absent_list in removed_list) is False
+    assert len(removed_list) == 0
+    assert correct == removed_list
+
+def test_group_random_extra():
+    """Testing the random type of grouping with a group of extra people not assigned to their own group"""
+    responses = [['Nick', True, False, True, False], ['Marvin', False, False, True, True], ['Evin', True, True, True, False], ['Nikki', True, True, False, False], ['Dan', False, True, False, True]]
+    grpsize = 2
+    returned_groups = group_random.group_random(responses, grpsize)
+    assert len(returned_groups) == 2
+    assert grpsize == 2
+
+def test_group_random():
+    """Testing the random type of grouping with everyone in an assigned group"""
+    responses = [['Nick', True, False, True, False], ['Marvin', False, False, True, True], ['Evin', True, True, True, False], ['Nikki', True, True, False, False], ['Dan', False, True, False, True], ['Michael', True, True, False, False]]
+    grpsize = 2
+    returned_groups = group_random.group_random(responses, grpsize)
+    assert len(returned_groups) == 3
