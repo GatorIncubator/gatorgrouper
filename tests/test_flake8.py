87951d80ea7f53fa63c16e646105851b6b699fbf
Mohammad Khan <mohammadnk1@gmail.com>
2019-02-12 18:06:30 -0500

    Commented out tests_flake8.py to see if hanging occurs

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -4,16 +4,16 @@ import os
 
 
 # Linting Tests
-def test_flake8():
-
-    # list of all file names to be checked for PEP8
-    filenames = list()
-
-    # fill list with all python files found in all subdirectories
-    for root, dirs, files in os.walk("gatorgrouper", topdown=False):
-        pyFiles = glob.glob(root + "/*.py")
-        filenames.extend(pyFiles)
-
-    style_guide = flake8.get_style_guide(ignore=["E265", "E501"])
-    report = style_guide.check_files(filenames)
-    assert report.get_statistics('E') == [], 'Flake8 found violations'
+# def test_flake8():
+#
+#     # list of all file names to be checked for PEP8
+#     filenames = list()
+#
+#     # fill list with all python files found in all subdirectories
+#     for root, dirs, files in os.walk("gatorgrouper", topdown=False):
+#         pyFiles = glob.glob(root + "/*.py")
+#         filenames.extend(pyFiles)
+#
+#     style_guide = flake8.get_style_guide(ignore=["E265", "E501"])
+#     report = style_guide.check_files(filenames)
+#     assert report.get_statistics('E') == [], 'Flake8 found violations'
