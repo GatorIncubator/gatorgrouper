e7cddf6deca59de8a6c45ec0840daf33eec1c0a7
Mohammad Khan <mohammadnk1@gmail.com>
2019-02-13 16:02:42 -0500

    Fixed all pylint logging issues in parse_arguments.py

--- parse_arguments.py
+++ parse_arguments.py
@@ -85,20 +85,20 @@ def parse_arguments(args):
     )
 
     if (
-        check_valid_group_size(
-            gg_arguments_finished.group_size,
-            read_student_file(gg_arguments_finished.students_file),
-        )
-        is False
+            check_valid_group_size(
+                gg_arguments_finished.group_size,
+                read_student_file(gg_arguments_finished.students_file),
+                )
+                is False
     ):
         quit()
 
     if (
-        check_valid_num_group(
-            gg_arguments_finished.num_group,
-            read_student_file(gg_arguments_finished.students_file),
-        )
-        is False
+            check_valid_num_group(
+                gg_arguments_finished.num_group,
+                read_student_file(gg_arguments_finished.students_file),
+                )
+                is False
     ):
         quit()
 
@@ -109,20 +109,21 @@ def parse_arguments(args):
 
 
 def check_valid_num_group(numgrp, students_list):
+    """Checking if valid num group"""
     if students_list == "filenotfound":
         logging.info("Skipping group size check; file must not exist.")
         return True
     students_list_length = len(students_list)
     if numgrp > students_list_length:
-        logging.error("Number of groups: " + str(numgrp))
-        logging.error("Number of students: " + str(students_list_length))
+        logging.error("Number of groups: %d", numgrp)
+        logging.error("Number of students: %d", students_list_length)
         logging.error(
             "Number of groups must be less than or equal to "
-            + "the number of students to be grouped."
+            "the number of students to be grouped. "
         )
         return False
-    logging.info("Number of groups: " + str(numgrp))
-    logging.info("Number of students: " + str(students_list_length))
+    logging.info("Number of groups: %d", numgrp)
+    logging.info("Number of students: %d", students_list_length)
     logging.info("Valid number of groups.")
     return True
 
@@ -134,15 +135,15 @@ def check_valid_group_size(group_size, students_list):
         return True
     students_list_length = len(students_list)
     if group_size <= 1 or group_size > students_list_length / 2:
-        logging.error("Group size: " + str(group_size))
-        logging.error("Number of students: " + str(students_list_length))
+        logging.error("Group size: %d", group_size)
+        logging.error("Number of students: %d", students_list_length)
         logging.error(
             "Group size must be greater than 1 and less than "
-            + "or equal to half of the number of students."
+            "or equal to half of the number of students."
         )
         return False
     # group size passed checks
-    logging.info("Group size: " + str(group_size))
-    logging.info("Number of students: " + str(students_list_length))
+    logging.info("Group size: %d", group_size)
+    logging.info("Number of students: %d", students_list_length)
     logging.info("Valid group size.")
     return True
