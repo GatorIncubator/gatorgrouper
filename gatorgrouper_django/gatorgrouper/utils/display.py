31461898bd1e5a8b76ecf310539b7671c01febc9
Isaac Barrezueta <barrezuetai@aldenv103.allegheny.edu>
2019-02-16 19:04:26 -0500

    restructured src code into utils directory with __init__.py file

---
+++ gatorgrouper_django/gatorgrouper/utils/display.py
@@ -0,0 +1,35 @@
+""" Various display functions """
+
+import logging
+
+
+def display_student_groups(student_groups):
+    """ Display the student groups with labels """
+    group_counter = 1
+    place_counter = 0
+    for student_group in student_groups:
+        #print("Group", group_counter)
+        print('\033[0;32m' + '\033[1m' + '\033[4m' + "Group", group_counter)
+        for student in student_group:
+            place_counter += 1
+            #print(student[0])
+            print('\033[0m' + student[0])   # converts the students back to original font
+        print()
+        group_counter = group_counter + 1
+    logging.info("Found " + str(place_counter) + " students")
+
+
+def display_welcome_message():
+    """ Display a welcome message """
+    print()
+    print("GatorGrouper: Automatically Assign Students to Groups")
+    print("https://github.com/GatorGrouper/gatorgrouper")
+    print()
+
+
+def create_escaped_string_from_list(student_identifers):
+    """ Return a string that lists the student identifiers """
+    student_list = ""
+    for student in student_identifers:
+        student_list = student_list + str(student) + "\n"
+    return student_list
---
+++ gatorgrouper_django/gatorgrouper/utils/spreadsheet.py
@@ -0,0 +1,56 @@
+""" Google Sheets Integration """
+
+import csv
+import logging
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from defaults import DEFAULT_CSVFILE
+from defaults import DEFAULT_WORKBOOK
+
+
+def create_csv():
+    """ Pulls data from Google Sheets, writing to the default CSV file """
+
+    file_name = "./" + DEFAULT_CSVFILE
+    logging.info(
+        "Authenticating to Google Sheets to obtain Google Form data")
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        'authorizationKey/keyFile.json', scope)
+    client = gspread.authorize(creds)
+
+    # Find a workbook by name and open the first sheet
+    # Make sure you use the right name here.
+    sheet = client.open(DEFAULT_WORKBOOK).sheet1
+
+    # Extract and print all of the values
+    list_of_hashes = sheet.get_all_records()
+
+    # Iterates through the list_of_hashes and creates a list of lists with the
+    # format ['username', True, False, True]
+
+    logging.info("Creating a list of lists of students")
+    formated_list = list()
+    for entry in list_of_hashes:
+        formated_entry = list()
+        for question, response in entry.items():
+            if question == 'Email Address':
+                username = entry[question].partition('@')[0]
+                formated_entry.append(username)
+            elif response == 'Yes':
+                formated_entry.append(True)
+            elif response == 'No':
+                formated_entry.append(False)
+        formated_entry.insert(
+            0, formated_entry.pop(
+                formated_entry.index(username)))
+        if not any(formated_entry[0] in entry[0] for entry in formated_list):
+            formated_list.append(formated_entry)
+
+    logging.info("Writing formatted data to CSV file")
+    logging.debug("CSV file name: " + file_name)
+    with open(file_name, 'w') as myfile:
+        writer = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for item in formated_list:
+            writer.writerow(item)
