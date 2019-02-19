1ceae03adb9ccd63a1a48249e32fce98643fd9c0
barrezuetai <ibarrezueta@Isaacs-MacBook-Pro.local>
2019-02-18 21:38:19 -0500

    Changed max length of teacher's first name and last name to 25 chars

--- gatorgrouper_django/gatorgrouper/models.py
+++ gatorgrouper_django/gatorgrouper/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Professor(models.Model):
     email = models.EmailField(max_length = 200)
     professor_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
+    first_name = models.CharField(max_length = 25)
+    last_name = models.CharField(max_length = 25)
 
     def __str__(self):
         return '{}'.format(self.email)
