7f4b98678c7a1dd60f4794e9957de851976ac1e9
Mohammad Khan <mohammadnk1@gmail.com>
2019-02-26 15:17:05 -0500

    Add `django-sass-processor` dependency. Set sass directory in settings.py. Me and @paladp are working on this.

--- gator_grouper/settings.py
+++ gator_grouper/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "sass_processor",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,4 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+SASS_PROCESSOR_ROOT = STATIC_ROOT
