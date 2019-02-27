174ed97a36eab85ff229ceeea189949584456a6b
enpuyou <youe2@allegheny.edu>
2019-02-27 00:36:33 -0500

    Added more test cases to cover models.py

--- gatorgrouper/tests/test_models.py
+++ gatorgrouper/tests/test_models.py
@@ -12,6 +12,54 @@ class TestProfessor:
 
     def test_str(self):
         obj = mixer.blend('gatorgrouper.Professor', last_name = 'K', first_name = 'Greg')
-        result = obj.__str__(self)
-        expected = 'Greg K'
+        result = obj.__str__()
+        expected = 'K, Greg'
+        assert result == expected
+
+class TestSemesterClss:
+    def test_model(self):
+        obj = mixer.blend('gatorgrouper.Semester_Class')
+        # it creates a semester class instance
+        assert obj.pk == 1
+
+    def test_str(self):
+        obj = mixer.blend('gatorgrouper.Semester_Class', department = 'CS', class_number = '203', class_section = '01')
+        result = obj.__str__()
+        expected = 'CS: 203*01'
+        assert result == expected
+
+class TestAssignments:
+    def test_model(self):
+        obj = mixer.blend('gatorgrouper.Assignments')
+        # it creates a assignment instance
+        assert obj.pk == 1
+
+    def test_str(self):
+        obj = mixer.blend('gatorgrouper.Assignments', assignment_id = 'Assignment One')
+        result = obj.__str__()
+        expected = 'Assignment One'
+        assert result == expected
+
+class TestStudents:
+    def test_model(self):
+        obj = mixer.blend('gatorgrouper.Students')
+        # it creates a student instance
+        assert obj.pk == 1
+
+    def test_str(self):
+        obj = mixer.blend('gatorgrouper.Students', last_name = 'Y', first_name = 'Enpu')
+        result = obj.__str__()
+        expected = 'Y, Enpu'
+        assert result == expected
+
+class TestGrouped_Students:
+    def test_model(self):
+        obj = mixer.blend('gatorgrouper.Grouped_Students')
+        # it creates a grouped students instance
+        assert obj.pk == 1
+
+    def test_str(self):
+        obj = mixer.blend('gatorgrouper.Grouped_Students', assignment_id = 'Assignment One', group_id = '02')
+        result = obj.__str__()
+        expected = 'Assignment One: 02'
         assert result == expected
