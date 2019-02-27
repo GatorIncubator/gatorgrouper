747aebd7a73218b9c113dba1e10adee1cb3d40f1
Lancaster Wu <theyueshen@gmail.com>
2019-02-27 02:30:12 -0500

    changed the way file testing is implemented

--- gatorgrouper/tests/test_read_student_file.py
+++ gatorgrouper/tests/test_read_student_file.py
@@ -4,4 +4,4 @@ from utils import read_student_file
 
 def test_read_file_populates_csvdata_0():
     """Checks that the read student file populates data from a CSV."""
-    assert len(read_student_file.read_student_file("gatorgrouper/tests/students.csv")) != 0
+    assert read_student_file.read_student_file("gatorgrouper/tests/students.csv") != 0
