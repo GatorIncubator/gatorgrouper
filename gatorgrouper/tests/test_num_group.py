72ea4950e5dcae701459351ccfcdf1947beb2453
Juncheng Wu <wuj@aldenv103.allegheny.edu>
2019-02-28 15:07:57 -0500

    added test function to test multi group number

--- gatorgrouper/tests/test_num_group.py
+++ gatorgrouper/tests/test_num_group.py
@@ -36,6 +36,26 @@ def test_check_number_groups_size_one():
     assert student_groups
 
 
+def test_check_number_groups_size_muti():
+    """Checking 1 group"""
+    numgrp = 2
+    numgrp2 = 3
+    student_identifiers = [
+        "maria",
+        "dan",
+        "simon",
+        "jesse",
+        "jon",
+        "michael",
+        "jacob",
+        "kapfhammer",
+    ]
+    student_groups = parse_arguments.check_valid_num_group(numgrp, student_identifiers)
+    student_groups2 = parse_arguments.check_valid_num_group(numgrp2, student_identifiers)
+    assert student_groups
+    assert student_groups2
+
+
 def test_check_number_groups_over():
     """Checking if the number of groups greater than number of students"""
     numgrp = 9
