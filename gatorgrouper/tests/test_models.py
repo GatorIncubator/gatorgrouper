8e9eec2f3f324d91dfdfe8e5fad8156515eab8a5
enpuyou <youe2@allegheny.edu>
2019-02-28 20:53:42 -0500

    Added blank lines and more docstrings

--- gatorgrouper/tests/test_models.py
+++ gatorgrouper/tests/test_models.py
@@ -4,67 +4,82 @@ import pytest
 from mixer.backend.django import mixer
 pytestmark = pytest.mark.django_db
 
+
 class TestProfessor:
     """test professor class"""
     def test_model(self):
+        """test professor model"""
         obj = mixer.blend('gatorgrouper.Professor')
         # it creates a professor instance
         assert obj.pk == 1
 
     def test_str(self):
+        """test __str__ method """
         obj = mixer.blend('gatorgrouper.Professor', last_name='K', first_name='Greg')
         result = obj.__str__()
         expected = 'K, Greg'
         assert result == expected
 
+
 class TestSemesterClss:
     """test semester class"""
     def test_model(self):
+        """test semester model"""
         obj = mixer.blend('gatorgrouper.Semester_Class')
         # it creates a semester class instance
         assert obj.pk == 1
 
     def test_str(self):
+        """test __str__ method """
         obj = mixer.blend('gatorgrouper.Semester_Class',
                           department='CS', class_number='203', class_section='01')
         result = obj.__str__()
         expected = 'CS: 203*01'
         assert result == expected
 
+
 class TestAssignments:
-    """test semester class"""
+    """test assignment class"""
     def test_model(self):
+        """test assignment model"""
         obj = mixer.blend('gatorgrouper.Assignments')
         # it creates a assignment instance
         assert obj.pk != " "
 
     def test_str(self):
+        """test __str__ method """
         obj = mixer.blend('gatorgrouper.Assignments', assignment_id='Assignment One')
         result = obj.__str__()
         expected = 'Assignment One'
         assert result == expected
 
+
 class TestStudents:
     """test student class"""
     def test_model(self):
+        """test student model"""
         obj = mixer.blend('gatorgrouper.Students')
         # it creates a student instance
         assert obj.pk == 1
 
     def test_str(self):
+        """test __str__ method """
         obj = mixer.blend('gatorgrouper.Students', last_name='Y', first_name='Enpu')
         result = obj.__str__()
         expected = 'Y, Enpu'
         assert result == expected
 
+
 class TestGrouped_Students:
     """test grouped student class"""
     def test_model(self):
+        """test grouped student model"""
         obj = mixer.blend('gatorgrouper.Grouped_Students')
         # it creates a grouped students instance
         assert obj.pk == 1
 
     def test_str(self):
+        """test __str__ method """
         obj = mixer.blend('gatorgrouper.Grouped_Students')
         result = obj.__str__()
         assert str(obj) == result
