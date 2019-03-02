e77aa4694b0ae63e376790e40753cfeb96aabcae
Aubrey Collins <collinsa@allegheny.edu>
2019-03-02 15:11:37 -0500

    Compute total cut size of k-way partition

--- gatorgrouper/utils/group_graph.py
+++ gatorgrouper/utils/group_graph.py
@@ -7,7 +7,7 @@ from networkx.algorithms.cuts import cut_size
 
 def recursive_kl(graph: Graph, num_groups=2) -> List[Set[int]]:
     """
-    Recursively use Kernighan-Lin bisection to create a k-way graph partition
+    Recursively use Kernighan-Lin algorithm to create a k-way graph partition
     """
     power = log(num_groups, 2)
     if power != int(power) or power < 1:
@@ -21,6 +21,18 @@ def recursive_kl(graph: Graph, num_groups=2) -> List[Set[int]]:
             subgraph = graph.subgraph(subset)
             groups += recursive_kl(subgraph, num_groups=next_num_groups)
         return groups
+
+
+def total_cut_size(graph: Graph, partition: List[int]) -> float:
+    """
+    Computes the sum of weights of all edges between different subsets in the partition
+    """
+    cut = 0.0
+    for i, subset1 in enumerate(partition):
+        for j, subset2 in enumerate(partition[i:]):
+            cut += cut_size(graph, subset1, T=subset2)
+            print(subset1, subset2, cut)
+    return cut
 
 
 def compatibility(a: Tuple[int], b: Tuple[int], preferences=[]) -> int:
@@ -57,15 +69,23 @@ def group_graph_partition(
     groups = []
     print("Partition:")
     for p in partition:
-        groups += [responses[i] for i in p]
+        groups.append([responses[i] for i in p])
         print([G.nodes[i]["weight"] for i in p])
-    # TODO: this is only the total edge cut size when we are bipartitioning...
-    print("Total cut size:", cut_size(G, partition[0], weight="weight"))
+    print("Total cut size:", total_cut_size(G, partition, weight="weight"))
     return groups
 
 
+def group_graph_partition_multiobjective(
+    responses: List[str], weights: List[Tuple[int]], grpsize: int, preferences=[]
+) -> List[str]:
+    if not weights:
+        return
+    objective_weights = [1] * len(weights[0])
+    if not preferences:
+        preferences = [1] * len(weights[0])
+
+
 if __name__ == "__main__":
-    # Create graph and populate with node weights
     students = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
     weights = [
         (0, 0),
@@ -77,4 +97,5 @@ if __name__ == "__main__":
         (1, 0),
         (1, 1),
     ]
-    group_graph_partition(students, weights, 4)
+    groups = group_graph_partition(students, weights, 4)
+    print(groups)
