f0a3aad2d11097f5134547b2544d009d4c81c6a0
chenc-allegheny <35603883+chenc-allegheny@users.noreply.github.com>
2019-03-03 18:03:45 -0500

    fix spaces

--- gatorgrouper/urls.py
+++ gatorgrouper/urls.py
@@ -1,6 +1,6 @@
 """ This is undocumented """
 from django.urls import path
-from django.urls import re_path
+# from django.urls import re_path
 
 # from django.conf import settings
 # from django.conf.urls.static import static
@@ -8,9 +8,9 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name="index"),
-    path('', views.home, name='Gtorgrouper-home'), # first attribute is a space, meaning homepage
+    path('', views.home, name='Gtorgrouper-home'),  # first attribute is a space, meaning homepage
     path('classes', views.classes, name='Gtorgrouper-classes'),
     path('assignments', views.assignments, name='Gatorgrouper-assignments'),
     path('survey', views.survey, name='Gatorgrouper-survey'),
     path('group-result', views.groupResult, name='Gatorgrouper-groups')
-    ]
+                ]
