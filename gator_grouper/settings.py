64c161ccf373c84d62584c9ef833fb433e15203d
chenc-allegheny <35603883+chenc-allegheny@users.noreply.github.com>
2019-03-03 18:10:20 -0500

    add blank lines

--- gator_grouper/settings.py
+++ gator_grouper/settings.py
@@ -64,6 +64,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.github.GithubOAuth2',
 
+    
     'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -72,6 +73,7 @@ def find_or_create_secret_key():
     Look for secret_key.py and return the SECRET_KEY entry in it if the file exists.
     Otherwise, generate a new secret key, save it in secret_key.py, and return the key.
     """
+    
     SECRET_KEY_DIR = os.path.dirname(__file__)
     SECRET_KEY_FILEPATH = os.path.join(SECRET_KEY_DIR, 'secret_key.py')
     sys.path.insert(1,SECRET_KEY_DIR)
@@ -85,6 +87,8 @@ def find_or_create_secret_key():
         new_key = get_random_string(50, chars)
         with open(SECRET_KEY_FILEPATH, 'w') as f:
             f.write("# Django secret key\n# Do NOT check this into version control.\n\nSECRET_KEY = '%s'\n" % new_key)
+        
+        
         from secret_key import SECRET_KEY
         return SECRET_KEY
 # Make this unique, and do not share it with anybody.
