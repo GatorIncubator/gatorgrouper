14ba9265e040a91b557e73b64eedf8c54bd225ff
Dillon Thoma <thomad@aldenv128.allegheny.edu>
2019-03-04 18:08:19 -0500

    Exception handling for a csv file not containing a header

--- gatorgrouper/utils/read_student_file.py
+++ gatorgrouper/utils/read_student_file.py
@@ -14,16 +14,34 @@ def read_student_file(filepath):
     # read the raw CSV data
     with open(filepath, "r") as csvfile:
         csvdata = list(csv.reader(csvfile, delimiter=","))
+        sniffer = csv.Sniffer()
+        has_header = sniffer.has_header(str(csvdata))
 
     # transform into desired output
     responses = list()
-    for record in csvdata[1:]:
-        temp = list()
-        temp.append(record[0].replace('"', ""))
-        for value in record[1:]:
-            if value == "True":
-                temp.append(True)
-            elif value == "False":
-                temp.append(False)
-        responses.append(temp)
+    # with open('students.csv', 'rb') as csvfile:
+    #     sniffer = csv.Sniffer()
+    #     has_header = sniffer.has_header(csvfile.read(2048))
+    #     csvfile.seek(0)
+    # handling files not containing headers
+    if has_header is True:
+        for record in csvdata[1:]:
+            temp = list()
+            temp.append(record[0].replace('"', ""))
+            for value in record[1:]:
+                if value == "True":
+                    temp.append(True)
+                elif value == "False":
+                    temp.append(False)
+            responses.append(temp)
+    else:
+        for record in csvdata:
+            temp = list()
+            temp.append(record[0].replace('"', ""))
+            for value in record[1:]:
+                if value == "True":
+                    temp.append(True)
+                elif value == "False":
+                    temp.append(False)
+            responses.append(temp)
     return responses
