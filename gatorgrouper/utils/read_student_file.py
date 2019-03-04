2b9db3ba98399cf32d52f84ff122a169dc0fc390
Spencer Huang <huangs@allegheny.edu>
2019-03-04 18:09:05 -0500

    Delete bad implementation of file not found exception handling

--- gatorgrouper/utils/read_student_file.py
+++ gatorgrouper/utils/read_student_file.py
@@ -7,9 +7,6 @@ from pathlib import Path
 def read_student_file(filepath):
     """ Read the responses from the CSV, returning them in a list of lists """
 
-    # handle nonexistant files
-    if Path(filepath).is_file() is False:
-        return "filenotfound"
 
     # read the raw CSV data
     with open(filepath, "r") as csvfile:
