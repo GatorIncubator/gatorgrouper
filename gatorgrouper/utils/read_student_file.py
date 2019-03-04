8fefd6416aa0bb16e7b268293e743e17e98d50d8
Dillon Thoma <thomad@aldenv128.allegheny.edu>
2019-03-04 18:17:10 -0500

    Changing function name to differ from the file name

--- gatorgrouper/utils/read_student_file.py
+++ gatorgrouper/utils/read_student_file.py
@@ -4,7 +4,7 @@ import csv
 from pathlib import Path
 
 
-def read_student_file(filepath):
+def read_csv_data(filepath):
     """ Read the responses from the CSV, returning them in a list of lists """
 
     # handle nonexistant files
@@ -19,11 +19,6 @@ def read_student_file(filepath):
 
     # transform into desired output
     responses = list()
-    # with open('students.csv', 'rb') as csvfile:
-    #     sniffer = csv.Sniffer()
-    #     has_header = sniffer.has_header(csvfile.read(2048))
-    #     csvfile.seek(0)
-    # handling files not containing headers
     if has_header is True:
         for record in csvdata[1:]:
             temp = list()
