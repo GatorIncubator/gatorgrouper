a954957107a3b083ea405f5bba4fd39d81e610e5
Lancaster Wu <theyueshen@gmail.com>
2019-03-05 00:47:19 -0500

    ran black

--- gatorgrouper/tests/test_read_student_file.py
+++ gatorgrouper/tests/test_read_student_file.py
@@ -27,7 +27,13 @@ def generate_csv(tmpdir_factory):
 def generate_csv_no_header(tmpdir_factory):
     """ Generate a tempory sample csv """
     fn = tmpdir_factory.mktemp("data").join("csvNg1.csv")
-    df_list = {"NAME": ["delgrecoj"], "Q1": [True], "Q2": [True], "Q3": [False], "Q4": [True]}
+    df_list = {
+        "NAME": ["delgrecoj"],
+        "Q1": [True],
+        "Q2": [True],
+        "Q3": [False],
+        "Q4": [True],
+    }
     df = pd.DataFrame(df_list)
     df.to_csv(str(fn), index=False, header=False)
     return str(fn)
