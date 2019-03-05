142c838f2e2390f2ea44dec70b67cc57b19a294e
Aubrey Collins <collinsa@allegheny.edu>
2019-03-05 15:28:05 -0500

    URL pattern for upload_csv

--- gatorgrouper/urls.py
+++ gatorgrouper/urls.py
@@ -1,9 +1,12 @@
 """ This is undocumented """
 # from django.urls import path,
-from django.urls import re_path
+from django.urls import path, re_path
 
 # from django.conf import settings
 # from django.conf.urls.static import static
 from . import views
 
-urlpatterns = [re_path(r"^$", views.index, name="index")]
+urlpatterns = [
+    re_path(r"^$", views.index, name="index"),
+    path("group/", views.upload_csv, name="upload_csv"),
+]
