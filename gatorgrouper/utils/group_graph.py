d9dd107b70454d7b3b2ffa10dde48e39d3b63527
Aubrey Collins <collinsa@allegheny.edu>
2019-03-06 15:41:56 -0500

    Refactor group_graph to pass pylint

--- gatorgrouper/utils/group_graph.py
+++ gatorgrouper/utils/group_graph.py
@@ -1,3 +1,4 @@
+""" Functions for creating groups via graph partitioning """
 from typing import List, Set, Tuple
 from math import log
 from networkx import Graph
@@ -5,22 +6,22 @@ from networkx.algorithms.community import kernighan_lin_bisection
 from networkx.algorithms.cuts import cut_size
 
 
-def recursive_kl(graph: Graph, num_groups=2) -> List[Set[int]]:
+def recursive_kl(graph: Graph, numgrp=2) -> List[Set[int]]:
     """
     Recursively use Kernighan-Lin algorithm to create a k-way graph partition
     """
-    power = log(num_groups, 2)
+    power = log(numgrp, 2)
     if power != int(power) or power < 1:
-        raise ValueError("num_groups must be a power of 2 and at least 2.")
-    if num_groups == 2:
+        raise ValueError("numgrp must be a power of 2 and at least 2.")
+    if numgrp == 2:
+        # Base case for recursion: use Kernighan-Lin to create 2 groups
         return list(kernighan_lin_bisection(graph))
-    else:
-        next_num_groups = num_groups / 2
-        groups = []
-        for subset in kernighan_lin_bisection(graph):
-            subgraph = graph.subgraph(subset)
-            groups += recursive_kl(subgraph, num_groups=next_num_groups)
-        return groups
+    next_numgrp = numgrp / 2
+    groups = []
+    for subset in kernighan_lin_bisection(graph):
+        subgraph = graph.subgraph(subset)
+        groups.extend(recursive_kl(subgraph, numgrp=next_numgrp))
+    return groups
 
 
 def total_cut_size(graph: Graph, partition: List[int]) -> float:
@@ -29,13 +30,13 @@ def total_cut_size(graph: Graph, partition: List[int]) -> float:
     """
     cut = 0.0
     for i, subset1 in enumerate(partition):
-        for j, subset2 in enumerate(partition[i:]):
+        for subset2 in partition[i:]:
             cut += cut_size(graph, subset1, T=subset2)
             print(subset1, subset2, cut)
     return cut
 
 
-def compatibility(a: Tuple[int], b: Tuple[int], preferences=[]) -> int:
+def compatibility(a: Tuple[int], b: Tuple[int], preferences=None) -> int:
     """
     Returns a score representing the compatibility between student a and student b.
     The maximum of each a[i] and b[i] in the tuples is scaled by preferences[i],
@@ -43,15 +44,18 @@ def compatibility(a: Tuple[int], b: Tuple[int], preferences=[]) -> int:
     """
     if not len(a) == len(b):
         raise Exception("Tuples passed to compatibility() must have same size")
-    if not preferences:
+    if preferences is None:
         preferences = [1] * len(a)
     adjusted_scores = [max(_a, _b) * _p for _a, _b, _p in zip(a, b, preferences)]
     return sum(adjusted_scores)
 
 
 def group_graph_partition(
-    responses: List[str], weights: List[Tuple[int]], grpsize: int
-) -> List[str]:
+        responses: List[str], weights: List[Tuple[int]], numgrp=2
+    ) -> List[str]:
+    """
+    Form groups using recursive Kernighan-Lin algorithm
+    """
     # Create graph and populate with node weights
     vertex_weight_pairs = enumerate([{"weight": w} for w in weights])
     G = Graph()
@@ -65,29 +69,19 @@ def group_graph_partition(
             print(i, j, w1, w2, score)
 
     # Partition the vertices
-    partition = recursive_kl(G, num_groups=2)
+    partition = recursive_kl(G, numgrp=numgrp)
     groups = []
     print("Partition:")
     for p in partition:
         groups.append([responses[i] for i in p])
         print([G.nodes[i]["weight"] for i in p])
-    print("Total cut size:", total_cut_size(G, partition, weight="weight"))
+    print("Total cut size:", total_cut_size(G, partition))
     return groups
-
-
-def group_graph_partition_multiobjective(
-    responses: List[str], weights: List[Tuple[int]], grpsize: int, preferences=[]
-) -> List[str]:
-    if not weights:
-        return
-    objective_weights = [1] * len(weights[0])
-    if not preferences:
-        preferences = [1] * len(weights[0])
 
 
 if __name__ == "__main__":
     students = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
-    weights = [
+    student_weights = [
         (0, 0),
         (0, 0.5),
         (0.5, 0),
@@ -97,5 +91,5 @@ if __name__ == "__main__":
         (1, 0),
         (1, 1),
     ]
-    groups = group_graph_partition(students, weights, 4)
-    print(groups)
+    student_groups = group_graph_partition(students, student_weights, 4)
+    print(student_groups)
