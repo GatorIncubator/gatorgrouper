47bff618aff25981d23883d567f3a4bb6dda664b
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-06 15:26:33 -0500

    imported useradmin class, and made professor auth user

--- gatorgrouper/admin.py
+++ gatorgrouper/admin.py
@@ -1,11 +1,12 @@
 """ This is undocumented """
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Professor, Semester_Class, Students, Grouped_Students, Assignments
 
 
 # Register your models here.
 
-admin.site.register(Professor)
+admin.site.register(Professor, UserAdmin)
 admin.site.register(Semester_Class)
 admin.site.register(Students)
 admin.site.register(Grouped_Students)
