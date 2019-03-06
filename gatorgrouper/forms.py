3d2f24d90d202776bd2c12cd6b4d8fa17f52add9
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-06 16:28:33 -0500

    created costomusercreationform and customuserchangeform

--- gatorgrouper/forms.py
+++ gatorgrouper/forms.py
@@ -1,3 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = Professor
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Professor
+        fields = ('username', 'email', 'first_name', 'last_name')
