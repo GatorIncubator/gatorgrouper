45e5cd652c658bfea12e0eb8fec8a4b7e64c83b5
Lancasterwu <wuj@allegheny.edu>
2019-03-05 23:57:34 -0500

    fixed more linting errors

--- gatorgrouper/utils/read_student_file.py
+++ gatorgrouper/utils/read_student_file.py
@@ -18,20 +18,19 @@ def read_student_file(filepath):
     if Path(filepath).is_file() is False:
         print("filenotfound")
         return ""
-    else:
-        # read the raw CSV data
-        with open(filepath, "r") as csvfile:
-            csvdata = list(csv.reader(csvfile, delimiter=","))
+    # read the raw CSV data
+    with open(filepath, "r") as csvfile:
+        csvdata = list(csv.reader(csvfile, delimiter=","))
 
         # transform into desired output
-        responses = list()
-        for record in csvdata[1:]:
-            temp = list()
-            temp.append(record[0].replace('"', ""))
-            for value in record[1:]:
-                if value == "True":
-                    temp.append(True)
-                elif value == "False":
-                    temp.append(False)
-            responses.append(temp)
-        return responses
+    responses = list()
+    for record in csvdata[1:]:
+        temp = list()
+        temp.append(record[0].replace('"', ""))
+        for value in record[1:]:
+            if value == "True":
+                temp.append(True)
+            elif value == "False":
+                temp.append(False)
+    responses.append(temp)
+    return responses
