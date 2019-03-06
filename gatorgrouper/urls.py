7455e235c8a048ff41693e1e60cd7aeefde45d00
Chih-Jung Chen <chenc@aldenv101.allegheny.edu>
2019-03-05 19:27:23 -0500

    fix typo

--- gatorgrouper/urls.py
+++ gatorgrouper/urls.py
@@ -10,9 +10,9 @@ from . import views
 urlpatterns = [
     path("index", views.index, name="index"),
     path(
-        "", views.home, name="Gtorgrouper-home"
+        "", views.home, name="Gatorgrouper-home"
     ),  # first attribute is a space, meaning homepage
-    path("classes", views.create_classes, name="Gtorgrouper-classes"),
+    path("classes", views.create_classes, name="Gatorgrouper-classes"),
     path("assignments", views.assignments, name="Gatorgrouper-assignments"),
     path("survey", views.survey, name="Gatorgrouper-survey"),
     path("group-result", views.groupResult, name="Gatorgrouper-groups"),
