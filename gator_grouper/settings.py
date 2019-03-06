d9a6f602022ec9d4b03a9af456dfd6672e2d8995
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-06 15:33:22 -0500

    Added prof auth in settings.py

--- gator_grouper/settings.py
+++ gator_grouper/settings.py
@@ -135,6 +135,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'gatorgrouper.Professor'
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
