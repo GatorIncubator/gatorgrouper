3ba766dfa67902c10686ff348aaab6dc5b67fa11
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-06 15:32:33 -0500

    changed name in admin.py class to reflect changes of model names

--- gatorgrouper/admin.py
+++ gatorgrouper/admin.py
@@ -1,13 +1,13 @@
 """ This is undocumented """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Professor, Semester_Class, Students, Grouped_Students, Assignments
+from .models import Professor, Semester_Class, Student, Grouped_Student, Assignment
 
 
 # Register your models here.
 
 admin.site.register(Professor, UserAdmin)
 admin.site.register(Semester_Class)
-admin.site.register(Students)
-admin.site.register(Grouped_Students)
-admin.site.register(Assignments)
+admin.site.register(Student)
+admin.site.register(Grouped_Student)
+admin.site.register(Assignment)
