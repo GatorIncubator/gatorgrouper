67eecada05ef5f760d09a40c51de54e2e7f16309
Spencer Huang <31478964+huangs1@users.noreply.github.com>
2019-03-06 13:48:32 -0500

    Fix some pylint errors

--- gatorgrouper/tests/test_group_method.py
+++ gatorgrouper/tests/test_group_method.py
@@ -3,11 +3,11 @@ from hypothesis import given
 from hypothesis import settings
 from hypothesis import Verbosity
 from hypothesis.strategies import integers
-from utils import group_random
-from utils import group_rrobin
 
 
 import pytest
+from utils import group_random
+from utils import group_rrobin
 
 
 def test_group_random1():
@@ -50,7 +50,7 @@ def test_group_random1():
 @given(group_size=integers(min_value=1, max_value=3))
 @settings(verbosity=Verbosity.verbose, deadline=None)
 @pytest.mark.hypothesisworks
-def test_group_random1(group_size):
+def hypothesis_test_group_random1(group_size):
     """this hypothesis test can generate the group numbers and test if it pass
         the requirements"""
     list = [
@@ -71,9 +71,9 @@ def test_group_random1(group_size):
     actual_output = group_random.group_random_group_size(list, group_size)
     actual_output2 = group_random.group_random_group_size(list2, group_size)
 
-    assert len(actual_output) == 12/size_count
+    assert len(actual_output) == 12 / size_count
     assert len(actual_output[0]) == size_count
-    assert len(actual_output2) == 6/size_count
+    assert len(actual_output2) == 6 / size_count
     assert len(actual_output2[0]) == size_count
 
 
