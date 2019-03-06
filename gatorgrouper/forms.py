cbe7554e25b54151af06297b925f4fc34791d13f
Aubrey Collins <collinsa@allegheny.edu>
2019-03-05 23:29:00 -0500

    Add numgrp field to UploadCSVForm

--- gatorgrouper/forms.py
+++ gatorgrouper/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class UploadCSVForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField() # CSV file to be uploaded & parsed
+    numgrp = forms.IntegerField(min_value=2, max_value=25) # Number of groups to create
