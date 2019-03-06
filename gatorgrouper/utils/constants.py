96528c83fcdab30dc21ed56afe1fb05f0882603f
Lancasterwu <wuj@allegheny.edu>
2019-03-05 21:19:34 -0500

    Added the variafy function in parsearg

--- gatorgrouper/utils/constants.py
+++ gatorgrouper/utils/constants.py
@@ -1,8 +1,10 @@
 """ These are global constants """
 
 ALGORITHM_ROUND_ROBIN = "rrobin"
+ALGORITHM_RANDOM = "random"
 DEFAULT_METHOD = "random"
 DEFAULT_CSVFILE = "students.csv"
 DEFAULT_GRPSIZE = 3
-DEFAULT_NUMGRP = 0
+DEFAULT_NUMGRP = 3
 DEFAULT_WORKBOOK = "Compsci280 Lab4 Survey Results"
+NONE = ""
--- gatorgrouper/utils/parse_arguments.py
+++ gatorgrouper/utils/parse_arguments.py
@@ -34,7 +34,7 @@ def parse_arguments(args):
     )
 
     gg_parser.add_argument(
-        "--group-size",
+        "--groupsize",
         required=True,
         help="Number of students in a group",
         type=int,
@@ -42,7 +42,7 @@ def parse_arguments(args):
     )
 
     gg_parser.add_argument(
-        "--num-group",
+        "--numgroup",
         required=True
         help="Number of groups",
         type=int,
@@ -50,17 +50,18 @@ def parse_arguments(args):
     )
 
     parser.add_argument(
-        "--file",
-        required=True,
-        type=str,
-        help="Package directory with functions to analyze",
-    )
-    
-    parser.add_argument(
         "--folder",
         required=True,
         type=str,
-        help="Module name with functions to analyze",
+        help="Input the directory",
+    )
+
+    parser.add_argument(
+        "--file",
+        verified_arguments = True
+        required=True,
+        type=str,
+        help="Input the file path",
     )
 
     gg_parser.add_argument(
@@ -68,49 +69,37 @@ def parse_arguments(args):
         help="Use random grouping method",
         action="store_const",
         dest="grouping_method",
-        const="random",
+        const=constants.ALGORITHM_RANDOM,
     )
 
     gg_parser.add_argument(
-        "--round-robin",
+        "--rrobin",
         help="Use round-robin grouping method",
         action="store_const",
         dest="grouping_method",
         const=constants.ALGORITHM_ROUND_ROBIN,
     )
 
-    gg_parser.add_argument("--absentees", nargs="+", type=str)
-
     gg_arguments_finished = gg_parser.parse_args(args)
 
-    logging.basicConfig(
-        format="%(levelname)s:%(pathname)s: %(message)s",
-        level=gg_arguments_finished.logging_level,
-    )
-
-    # pylint: disable=bad-continuation
-    if (
-        check_valid_group_size(
-            gg_arguments_finished.group_size,
-            read_student_file.read_student_file(gg_arguments_finished.students_file),
-        )
-        is False
-    ):
-        quit()
-
-    if (
-        check_valid_num_group(
-            gg_arguments_finished.num_group,
-            read_student_file.read_student_file(gg_arguments_finished.students_file),
-        )
-        is False
-    ):
-        quit()
-
-    if gg_arguments_finished.absentees is None:
-        gg_arguments_finished.absentees = []
-
     return gg_arguments_finished
+
+
+def check_valid(args):
+    """Verify the command-line arguments"""
+    verified_arguments = False
+    # CHECK: folder was specified and it is not ""
+    if args.folder is not constants.NONE:
+        verified_arguments = True
+    # CHECK: file was specified and it is not ""
+    if args.file is not constants.NONE:
+        verified_arguments = True
+    # CHECK: number was specified and it is not ""
+    if args.groupsize is not constants.NONE:
+        verified_arguments = True
+    if args.numgroup is not constants.NONE:
+        verified_arguments = True
+    return verified_arguments
 
 
 def check_valid_num_group(numgrp, students_list):
