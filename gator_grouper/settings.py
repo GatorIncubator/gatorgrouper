1bde26ac37c6832b144608eb683540083c918503
barrezuetai <barrezuetai@allegheny.edu>
2019-03-07 14:33:03 -0500

    Added login url key

--- gator_grouper/settings.py
+++ gator_grouper/settings.py
@@ -164,5 +164,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 LOGIN_REDIRECT_URL = 'Gatorgrouper-home'
+
+LOGIN_URL = 'login'
