adc3110490ee76d6cf6c095755b796cc6f17bcfc
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-06 19:09:27 -0500

    created custom UserAdmin class that overrides Djangos UserAdmin class

--- gatorgrouper/admin.py
+++ gatorgrouper/admin.py
@@ -1,12 +1,33 @@
 """ This is undocumented """
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 from .models import Professor, Semester_Class, Student, Grouped_Student, Assignment
 
 
 # Register your models here.
 
-admin.site.register(Professor, UserAdmin)
+@admin.register(Professor)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
 admin.site.register(Semester_Class)
 admin.site.register(Student)
 admin.site.register(Grouped_Student)
