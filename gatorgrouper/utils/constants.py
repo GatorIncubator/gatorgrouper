8df87170e762de708d14826e0b8b93aaac82ef3e
LancasterDesktop <wuj@allegheny.edu>
2019-03-06 23:39:52 -0500

    orgnized the constants

--- gatorgrouper/utils/constants.py
+++ gatorgrouper/utils/constants.py
@@ -1,16 +1,16 @@
 """ These are global constants """
 
-
+# default
 ALGORITHM_ROUND_ROBIN = "rrobin"
 ALGORITHM_RANDOM = "random"
 DEFAULT_METHOD = "random"
 DEFAULT_GRPSIZE = 3
 DEFAULT_NUMGRP = 3
-
 DEFAULT_ABSENT = ""
 
+# assertion
 NONE = ""
 
+# recognizer
 EMPTY_STRING = ""
-
 ERROR = "error:"
