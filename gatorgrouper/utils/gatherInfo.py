a90ef115373325f4ebfdc7fd8574b59ed44043e9
Haeley Griffin <griffinh@aldenv164.allegheny.edu>
2019-03-08 15:45:20 -0500

    conflicts gathered

--- gatorgrouper/utils/gatherInfo.py
+++ gatorgrouper/utils/gatherInfo.py
@@ -1,8 +1,17 @@
 """Gather user Info"""
 
 from .models import Professor, Semester_Class, Student
+from django.db.models import Q
 
 # given a professor and class, make a list of students
 def gatherinfo(request):
     current_professor = request.user
     students = list(Semester_Class.objects.filter(professor_id = current_professor))
+    return students
+
+def gatherinfoConflict():
+    listOfStudents = gatherinfo(request)
+    conflicts = []
+    for student in listOfStudents :
+         conflicts.append(Student_Conflicts.object.filter(Q(student1 = student) | Q(student2 = student)))
+    return conflicts
