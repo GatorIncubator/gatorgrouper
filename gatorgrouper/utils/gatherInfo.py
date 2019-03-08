f675546bf5cd015c8deaf388ec12147476d30c53
Sweta Rauniyar <rauniyars@aldenv107.allegheny.edu>
2019-03-07 23:37:53 -0500

    Added funstion to create the list of students

--- gatorgrouper/utils/gatherInfo.py
+++ gatorgrouper/utils/gatherInfo.py
@@ -1,1 +1,8 @@
 """Gather user Info"""
+
+from .models import Professor, Semester_Class, Student
+
+# given a professor and class, make a list of students
+def gatherinfo(request):
+    current_professor = request.user
+    students = list(Semester_Class.objects.filter(professor_id = current_professor))
