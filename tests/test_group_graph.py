92acae67b19aa6a5dae51751dd611a38e2c34107
Matthew Baldeosingh <baldeosinghm@allegheny.edu>
2019-03-09 22:58:01 -0500

    Started work on first test case of test/test_group_graph

--- tests/test_group_graph.py
+++ tests/test_group_graph.py
@@ -2,3 +2,9 @@
 # from gatorgrouper.utils import group_graph
 from networkx import Graph
 from gatorgrouper.utils import group_graph
+
+
+def test_recursive_kl():
+    """ Test if groups made match recursion"""
+    group = [{0, 2}, {4, 7}, {3, 5}, {1, 6}]
+    weights = [[0, 0], [0, 0.5], [0.5, 0], [0.75, 0.75], [0.8, 0.1], [0, 1], [1, 0], [1, 1]]
