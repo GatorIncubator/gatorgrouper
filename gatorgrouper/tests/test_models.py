8b528749aea39e93725b4683137fd7cb30b72fdd
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-11 13:45:24 -0400

    Fixed model name in test file

--- gatorgrouper/tests/test_models.py
+++ gatorgrouper/tests/test_models.py
@@ -60,7 +60,7 @@ class TestAssignments:
     def test_model(self):
         """test assignment model by creating a assignment instance and asserted
         that database is not empty"""
-        obj = mixer.blend("gatorgrouper.Assignments")
+        obj = mixer.blend("gatorgrouper.Assignment")
         # it creates a assignment instance
         assert obj.pk != " "
 
@@ -68,7 +68,7 @@ class TestAssignments:
     def test_str(self):
         """test __str__ method by putting varibles into the database and
         expecting the output to be in a specific format"""
-        obj = mixer.blend("gatorgrouper.Assignments", assignment_id="Assignment One")
+        obj = mixer.blend("gatorgrouper.Assignment", assignment_id="Assignment One")
         result = str(obj)
         expected = "Assignment One"
         assert result == expected
@@ -81,7 +81,7 @@ class TestStudents:
     def test_model(self):
         """test student model by creating a student instance and assert that
         there is one in the database"""
-        obj = mixer.blend("gatorgrouper.Students")
+        obj = mixer.blend("gatorgrouper.Student")
         # it creates a student instance
         assert obj.pk == 1
 
@@ -89,7 +89,7 @@ class TestStudents:
     def test_str(self):
         """test __str__ method by putting varibles into the database and
         expecting the output to be in a specific format"""
-        obj = mixer.blend("gatorgrouper.Students", last_name="Y", first_name="Enpu")
+        obj = mixer.blend("gatorgrouper.Student", last_name="Y", first_name="Enpu")
         result = str(obj)
         expected = "Y, Enpu"
         assert result == expected
@@ -102,14 +102,14 @@ class TestGrouped_Students:
     def test_model(self):
         """test grouped student model by creating a grouped student instance and
         assert that there is one in the database"""
-        obj = mixer.blend("gatorgrouper.Grouped_Students")
+        obj = mixer.blend("gatorgrouper.Grouped_Student")
         # it creates a grouped students instance
         assert obj.pk == 1
 
     # pylint: disable=R0201
     def test_str(self):
         """test __str__ method """
-        obj = mixer.blend("gatorgrouper.Grouped_Students")
+        obj = mixer.blend("gatorgrouper.Grouped_Student")
         result = str(obj)
         expectedsymbol = ":"
         assert expectedsymbol in result
