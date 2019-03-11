39edc03ca36efafcb4d8c5b8117508e55f9f442a
barrezuetai <barrezuetai@allegheny.edu>
2019-03-11 01:02:10 -0400

    Fixed admin.py post merge

--- gatorgrouper/admin.py
+++ gatorgrouper/admin.py
@@ -1,14 +1,9 @@
 """ This is undocumented """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
 from .models import Professor, Semester_Class, Student, Grouped_Student, Assignment
 
-=======
-from .models import Professor, Semester_Class, Students, Grouped_Students, Assignments
-from .models import Student_Conflicts
->>>>>>> student_conflicts_model
 
 # Register your models here.
 
@@ -34,13 +29,7 @@ class UserAdmin(DjangoUserAdmin):
     ordering = ('email',)
 
 admin.site.register(Semester_Class)
-<<<<<<< HEAD
-admin.site.register(Student)
-admin.site.register(Grouped_Student)
-admin.site.register(Assignment)
-=======
 admin.site.register(Students)
 admin.site.register(Grouped_Students)
 admin.site.register(Assignments)
 admin.site.register(Student_Conflicts)
->>>>>>> student_conflicts_model
