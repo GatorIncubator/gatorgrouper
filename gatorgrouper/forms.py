656c80e5e60eb32ccc13b456f54e2ae8ada9756a
enpuyou <youe2@allegheny.edu>
2019-03-11 17:05:39 -0400

    Fixed import errors in forms

--- gatorgrouper/forms.py
+++ gatorgrouper/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+# from .models import CustomUser
+from .models import Professor
 
 
 class CustomUserCreationForm(UserCreationForm):
