5f9c5782be354a6866a7b67f16ee3c7d71d04e20
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-03-11 15:46:42 -0400

    more flake8 changes

--- gatorgrouper/forms.py
+++ gatorgrouper/forms.py
@@ -8,16 +8,20 @@ from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
     """ New user creation form without username """
+
     class Meta(UserCreationForm):
         """ This class is undocumented """
+
         model = get_user_model()
         fields = ("email", "first_name", "last_name")
 
 
 class CustomUserChangeForm(UserChangeForm):
     """ Custom userchangeform for custom user """
+
     # pylint: disable=too-few-public-methods
     class Meta:
         """ This class is undocumented """
+
         model = get_user_model()
         fields = ("email", "first_name", "last_name")
--- gatorgrouper/utils/gatherInfo.py
+++ gatorgrouper/utils/gatherInfo.py
@@ -1,13 +1,13 @@
 """Gather user Info"""
 
 from django.db.models import Q
-from gatorgrouper.models import Student, Student_Conflict
+# pylint: disable=no-name-in-module
+from gatorgrouper.models import Student, Student_Conflict  # pylint: disable=import-error
 
 
 # given a professor and class, make a list of students
 def gatherStudents(current_class):
     """ gathers list of students in given class """
-    # pylint: disable=no-member
     students = list(Student.objects.filter(class_id=current_class))
     return students
 
@@ -18,7 +18,6 @@ def gatherConflicts(current_class):
     conflicts = []
     for student in listOfStudents:
         conflicts.append(
-            # pylint: disable=no-member
             Student_Conflict.object.filter(Q(student1=student) | Q(student2=student))
         )
     return conflicts
