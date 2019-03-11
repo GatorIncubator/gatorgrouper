dc44ea05db621ccb0a8eac039107105b945d76c3
enpuyou <youe2@allegheny.edu>
2019-03-11 17:01:00 -0400

    updated

--- gatorgrouper/urls.py
+++ gatorgrouper/urls.py
@@ -1,5 +1,4 @@
 """ This is undocumented """
-<<<<<<< HEAD
 from django.urls import path
 
 # from django.urls import re_path
@@ -14,18 +13,4 @@ urlpatterns = [
     path("assignments/", views.assignments, name="Gatorgrouper-assignments"),
     path("survey/", views.survey, name="Gatorgrouper-survey"),
     path("group-result/", views.groupResult, name="Gatorgrouper-groups"),
-=======
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url("group", views.upload_csv, name="upload_csv"),
-    url("classes", views.create_classes, name="Gatorgrouper-classes"),
-    url("assignments", views.assignments, name="Gatorgrouper-assignments"),
-    url("survey", views.survey, name="Gatorgrouper-survey"),
-    url("group-result", views.groupResult, name="Gatorgrouper-groups"),
-    url(
-        "", views.home, name="Gatorgrouper-home"
-    ),  # first attribute is a space, meaning homepage
->>>>>>> master
 ]
