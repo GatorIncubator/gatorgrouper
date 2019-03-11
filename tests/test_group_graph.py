f2b025f003f30b714e5e3ea5f44ecdfe52337f6d
Benjamin Watto <wattob@aldenv184.allegheny.edu>
2019-03-11 13:57:05 -0400

    Running Black for Formatting

--- tests/test_group_graph.py
+++ tests/test_group_graph.py
@@ -5,7 +5,16 @@ from networkx import Graph
 
 def test_recursive_kl():
     """ Test if groups made match recursion"""
-    weights = [[0, 0], [0, 0.5], [0.5, 0], [0.75, 0.75], [0.8, 0.1], [0, 1], [1, 0], [1, 1]]
+    weights = [
+        [0, 0],
+        [0, 0.5],
+        [0.5, 0],
+        [0.75, 0.75],
+        [0.8, 0.1],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+    ]
     vertex_weight_pairs = enumerate([{"weight": w} for w in weights])
     G = Graph()
     G.add_nodes_from(vertex_weight_pairs)
@@ -27,6 +36,15 @@ def test_main():
         ["seven", 1, 0],
         ["eight", 1, 1],
     ]
-    weights = [[0, 0], [0, 0.5], [0.5, 0], [0.75, 0.75], [0.8, 0.1], [0, 1], [1, 0], [1, 1]]
+    weights = [
+        [0, 0],
+        [0, 0.5],
+        [0.5, 0],
+        [0.75, 0.75],
+        [0.8, 0.1],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+    ]
     groups = group_graph.group_graph_partition(students, 4)
     assert weights in groups
