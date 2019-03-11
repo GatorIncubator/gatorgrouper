71ae805d9d4212313f465f736acc38e60ad5ad5b
Aubrey Collins <collinsa@allegheny.edu>
2019-03-10 20:43:50 -0400

    Update UploadCSVForm to have preferences input and restrict numgrp to power of 2

--- gatorgrouper/forms.py
+++ gatorgrouper/forms.py
@@ -5,7 +5,10 @@ from django import forms
 class UploadCSVForm(forms.Form):
     """ Form enabling the uploading of a CSV file to be used for grouping """
 
-    file = forms.FileField(label="Student data CSV file")
-    numgrp = forms.IntegerField(
-        min_value=2, max_value=25, label="Number of groups to create"
+    student_data = forms.FileField(label="Student data CSV file")
+    student_preferences = forms.FileField(label="Student preferences CSV file", required=False)
+    numgrp = forms.TypedChoiceField(
+        choices=[(str(2 ** i), str(2**i)) for i in range(1, 5)],  # Restrict choices to powers of 2
+        coerce=int,
+        label="Number of groups to create"
     )
