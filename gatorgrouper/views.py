f63b085c174436cb25af3364b191a3a05e526666
Aubrey Collins <collinsa@allegheny.edu>
2019-03-10 20:44:21 -0400

    Web interface for graph partition algorithm

--- gatorgrouper/views.py
+++ gatorgrouper/views.py
@@ -1,8 +1,9 @@
 """ This is undocumented """
 import csv
+import re
 from io import StringIO
 from django.shortcuts import render
-from .utils.group_rrobin import group_rrobin_num_group
+from .utils.group_graph import group_graph_partition
 from .forms import UploadCSVForm
 
 
@@ -11,9 +12,17 @@ def upload_csv(request):
     if request.method == "POST":
         form = UploadCSVForm(request.POST, request.FILES)
         if form.is_valid():
-            responses = handle_uploaded_file(request.FILES["file"])
+            responses = parse_uploaded_csv(request.FILES["student_data"])
+            if request.FILES.get("student_preferences"):
+                preferences = parse_uploaded_csv(request.FILES["student_preferences"], as_dict=True)
+            else:
+                preferences = None
             numgrp = form.cleaned_data["numgrp"]
-            groups = group_rrobin_num_group(responses, numgrp)
+            groups = group_graph_partition(
+                responses,
+                numgrp,
+                preferences=preferences
+            )
             return render(
                 request, "gatorgrouper/viewing-groups.html", {"groups": groups}
             )
@@ -22,17 +31,23 @@ def upload_csv(request):
     return render(request, "gatorgrouper/upload_csv.html", {"form": form})
 
 
-def handle_uploaded_file(csvfile):
+def parse_uploaded_csv(csvfile, as_dict=False):
     """
         Transform uploded CSV data into list of student responses:
-        [["student name", True, False, ...]]
+        [["student name", True, False, ...], ...]
+
+        With the as_dict parameter set to True, transforms the CSV data into a dictionary of sets:
+        {"student name": {True, False, ...}, ...}
     """
     f = StringIO(csvfile.read().decode("utf-8"))
     csvdata = list(csv.reader(f, delimiter=","))
 
     # transform into desired output
     responses = list()
+    responses_dict = {}
     for record in csvdata:
+        if as_dict:
+            responses_dict[record[0]] = set()  # Create key in responses dictionary
         temp = list()
         temp.append(record[0].replace('"', ""))
         for value in record[1:]:
@@ -40,8 +55,14 @@ def handle_uploaded_file(csvfile):
                 temp.append(True)
             elif value.lower() == "false":
                 temp.append(False)
+            elif re.match(r"[+-]?([0-9]*[.])?[0-9]+", value):  # Match a float with regex
+                temp.append(float(value))
+            else:  # Keep the value as a string if no other type matches
+                temp.append(value)
+            if as_dict:  # Assign the value to the responses set for this row
+                responses_dict[record[0]].add(value)
         responses.append(temp)
-    return responses
+    return responses if not as_dict else responses_dict
 
 
 def home(request):
