1d2d0990b59be1b2088fa572e336aac8a59e9a9b
Benjamin Watto <wattob@aldenv139.allegheny.edu>
2019-03-12 09:33:02 -0400

    Format Changes

--- tests/test_group_graph.py
+++ tests/test_group_graph.py
@@ -8,7 +8,7 @@ def test_recursive_kl():
     """ Test if groups made match recursion """
     G = Graph()
     with pytest.raises(ValueError) as excinfo:
-        group_graph.recursive_kl(G,numgrp=1)
+        group_graph.recursive_kl(G, numgrp=1)
         exception_msg = excinfo.value.args[0]
         assert exception_msg == "numgrp must be a power of 2 and at least 2."
 
@@ -19,7 +19,7 @@ def test_total_cut_size():
     """ Test if cut size of subsets in partition match """
     G = Graph()
     G.add_edges_from([(1, 4), (2, 3)])
-    partition = ([(1, 2), (3, 4)])
+    partition = [(1, 2), (3, 4)]
     output = group_graph.total_cut_size(G, partition)
     expected_output = 2
     assert output == expected_output
@@ -27,8 +27,8 @@ def test_total_cut_size():
 
 def test_compatibility_length():
     """ Test if exception message is raised by unequal students scores """
-    a = (2)
-    b = (1)
+    a = 2
+    b = 1
     with pytest.raises(Exception) as excinfo:
         group_graph.compatibility(a, b)
         exception_msg = excinfo.value.args[0]
@@ -37,6 +37,5 @@ def test_compatibility_length():
 
 def test_compatibility():
     """ Test compatibility between students """
-    a = (2)
-    b = (2)
-    
+    a = 2
+    b = 2
