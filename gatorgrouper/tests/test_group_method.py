697a47b5f4f076ed9d7700b0750aec3c3bccecf8
Matthew Marconi <marconim@aldenv158.allegheny.edu>
2019-03-12 15:37:08 -0400

    Add hypothesis test_random
    Co-authored-by: yeej2 <yeej2@allegheny.edu>

--- gatorgrouper/tests/test_group_method.py
+++ gatorgrouper/tests/test_group_method.py
@@ -305,3 +305,31 @@ def test_random():
     assert len(actual_output[0]) == 4
     assert len(actual_output1) == 3
     assert len(actual_output1[0]) == 4
+
+
+@given(grpsize=integers(min_value=1, max_value=4))
+@settings(verbosity=Verbosity.verbose)
+@pytest.mark.hypothesisworks
+def test_hypothesis_random(grpsize):
+   """Testing the random grouping function to assure proper output"""
+   lst = [
+       ["Dan", True, True, True],
+       ["Jesse", True, True, True],
+       ["Austin", True, True, True],
+       ["Nick", False, False, False],
+       ["Nikki", False, False, False],
+       ["Maria", False, False, False],
+       ["Jeff", False, False, False],
+       ["Simon", False, False, False],
+       ["Jon", False, False, False],
+       ["Angie", False, False, False],
+       ["Izaak", False, False, False],
+       ["Jacob", False, False, False],
+   ]
+   num_group = len(lst) // grpsize
+   actual_output = group_random.group_random_group_size(lst, grpsize)
+   actual_output1 = group_random.group_random_num_group(lst, num_group)
+   assert len(actual_output) == num_group
+   assert len(actual_output[0]) == grpsize
+   assert len(actual_output1) == num_group
+   assert len(actual_output1[0]) == grpsize
