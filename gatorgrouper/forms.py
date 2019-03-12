4965bf9ca483314e72e9e61f1c1f3f1b7d493e94
Sweta Rauniyar <rauniyars@aldenv137.allegheny.edu>
2019-03-12 11:42:07 -0400

    Added import statement to fix pytest error

--- gatorgrouper/forms.py
+++ gatorgrouper/forms.py
@@ -4,6 +4,7 @@ our custom User
 """
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
