be409610e6206899f322f9696abc477492a1a36b
Jared Szklenski <szklenskij@aldenv155.allegheny.edu>
2019-03-12 16:47:19 -0400

    Ran Black on gatorgrouper/

--- gatorgrouper/models.py
+++ gatorgrouper/models.py
@@ -50,6 +50,7 @@ class Assignments(models.Model):
 
 class Students(models.Model):
     """ This is undocumented """
+
     users = models.OneToOneField(User, on_delete=models.CASCADE)
     class_id = models.ForeignKey(Semester_Class, on_delete=models.CASCADE)
     student_id = models.AutoField(primary_key=True)
