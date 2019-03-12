dea64f0a7f9014db7b9345094f90c8aadf551282
Alexander Yarkosky <yarkoskya@allegheny.edu>
2019-03-12 13:31:49 -0400

    add algorithm for conflict scoring
    Co-authored-by: robert-samuel07 <roberts@allegheny.edu>

--- gatorgrouper/utils/group_scoring.py
+++ gatorgrouper/utils/group_scoring.py
@@ -30,3 +30,22 @@ def score_groups(
     ave = int(ave / len(student_groups))
     # returns the scores of each group as well as the average score
     return scores, ave
+
+
+#def score_group_conflicts(student_indentifiers):
+#    """ conflict score single group """
+#    conflict score = 0
+#    for student in student_identifiers:
+#
+#    return conflict_score
+
+
+def score_group_conflicts(student_groups):
+    """ score group conflicts """
+    conflict_scores = []
+    conflict_ave = 0
+    for group in student_groups:
+        conflict_scores.append(score_group_conflict(group))
+        conflict_ave += score_group_conflict(group)
+    conflict_ave = int(ave / len(student_groups))
+return conflict_scores, conflict_ave
