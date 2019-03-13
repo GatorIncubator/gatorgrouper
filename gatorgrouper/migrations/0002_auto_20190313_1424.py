5a075264964d8d465c90d5ba5378940c9e9bb0e0
barrezuetai <barrezuetai@allegheny.edu>
2019-03-13 12:25:32 -0400

    black fixes

--- gatorgrouper/migrations/0002_auto_20190313_1424.py
+++ gatorgrouper/migrations/0002_auto_20190313_1424.py
@@ -5,13 +5,10 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('gatorgrouper', '0001_initial'),
-    ]
+    dependencies = [("gatorgrouper", "0001_initial")]
 
     operations = [
         migrations.AlterUniqueTogether(
-            name='grouped_student',
-            unique_together={('assignment_id', 'student_id')},
-        ),
+            name="grouped_student", unique_together={("assignment_id", "student_id")}
+        )
     ]
