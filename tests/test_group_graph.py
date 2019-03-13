e9f0c6f4f308d77d9199c54e03be95cf219e23c0
Ben <31478967+wattob@users.noreply.github.com>
2019-03-13 15:22:49 -0400

    Edits to follow Pylint

--- tests/test_group_graph.py
+++ tests/test_group_graph.py
@@ -135,7 +135,10 @@ def test_compatability_measure_diff():
 
 
 def test_group_graph_partition():
-    """ Test for using recursive Kernighan-Lin algorithm that checks the output of the group_graph_partition function """
+    """
+    Test for using recursive Kernighan-Lin algorithm that checks the output of
+    the group_graph_partition function
+    """
     students = [
         ["one", 0, 0],
         ["two", 0, 0.5],
