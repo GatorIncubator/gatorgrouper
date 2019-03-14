7b9086ee170b195e3412129e110800ed0288e594
Spencer Huang <huangs@allegheny.edu>
2019-03-14 11:16:20 -0400

    Fix flake8 and pylint errors

--- gatorgrouper/views.py
+++ gatorgrouper/views.py
@@ -13,6 +13,7 @@ from .models import Semester_Class, Student
 from .models import Grouped_Student, Assignment
 from .utils.gatherInfo import gatherStudents
 from .utils.group_graph import group_graph_partition
+from .utils.group_rrobin import group_rrobin_num_group
 from .forms import UploadCSVForm, CreateGroupForm
 from .forms import CustomUserCreationForm
 
