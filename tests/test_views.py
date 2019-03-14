2d4792e83af24284fcc1eda584d6d73c99f9d522
enpuyou <youe2@allegheny.edu>
2019-03-14 15:19:03 -0400

    Added test upload csv form

--- tests/test_views.py
+++ tests/test_views.py
@@ -8,10 +8,33 @@ from django.test import TestCase
 from django.urls import reverse
 from mixer.backend.django import mixer
 from django.test.client import Client
-from gatorgrouper.forms import CustomUserCreationForm
+from gatorgrouper.forms import CustomUserCreationForm, UploadCSVForm
 
 pytestmark = pytest.mark.django_db
 
+
+class TestCustomUserCreationForm:
+    def test_valid_data(self):
+        form = CustomUserCreationForm({
+            "email": "testuserl@test.com",
+            "first_name": "Spencer",
+            "last_name": "Huang",
+            "password1": "testpassword1",
+            "password2": "testpassword1",
+        })
+        assert form.is_valid() is True
+
+
+class TestUploadCSVForm:
+    def test_valid_data(self, generate_csv):
+        form = UploadCSVForm({
+            "file": generate_csv,
+            "numgrp": 3,
+        })
+        # assert form.is_valid() is True
+
+        assert form.fields['file'].label == "Student data CSV file"
+        assert form.fields['numgrp'].label == "Number of groups to create"
 
 class TestView:
     def setup(self):
