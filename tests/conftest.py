c4493fdc342ace93e21b970c140c22640f5a47ee
enpuyou <youe2@allegheny.edu>
2019-03-14 17:54:01 -0400

    added a fixture to generate csv file

--- tests/conftest.py
+++ tests/conftest.py
@@ -135,3 +135,32 @@ def generate_csv_float_no_header(tmpdir_factory):
 def no_arguments():
     """Return no command-line arguments"""
     return []
+
+
+@pytest.fixture(scope="session")
+def generate_csv_file(tmpdir_factory):
+    """ Generate a tempory sample csv """
+    fn = tmpdir_factory.mktemp("data").join("csvNg.csv")
+    headers = ["NAME", "Q1", "Q2", "Q3", "Q4"]
+    with open(str(fn), "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        writer.writerow(
+            {
+                "NAME": "delgrecoj",
+                "Q1": "True",
+                "Q2": "True",
+                "Q3": "False",
+                "Q4": "True",
+            }
+        )
+        writer.writerow(
+            {
+                "NAME": "delgrecoj2",
+                "Q1": "True",
+                "Q2": "True",
+                "Q3": "False",
+                "Q4": "True",
+            }
+        )
+    return fn
