34f3a82b32bf7c1eb542e210c12ddefd59246628
Chih-Jung Chen <chenc@aldenv107.allegheny.edu>
2019-03-14 18:04:01 -0400

    83% need to test the generate random part

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,15 +1,9 @@
 """Testing settings"""
 import pytest
 import settings
+import os
 
 def test_find_or_create_secret_key():
     """test if key is 50 characters"""
-    key = settings.find_or_create_secret_key()
-    assert len(key) == 50
-
-def test_if_generate_random_key():
-    """see if it generates a ranfom key of key is not provided"""
-    settings.SECRET_KEY_DIR = None 
-    settings.SECRET_KEY_FILEPATH = None
     key = settings.find_or_create_secret_key()
     assert len(key) == 50
