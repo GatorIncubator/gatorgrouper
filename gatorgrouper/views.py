9e93b27f8adef653f41e88a3d6800ca20f88e6a9
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-17 00:28:30 -0400

    Fix black formatting in views.py

--- gatorgrouper/views.py
+++ gatorgrouper/views.py
@@ -76,10 +76,7 @@ def parse_uploaded_csv(csvfile, as_dict=False):
             elif value.lower() == "false":
                 temp.append(False)
             # pylint: disable=bad-continuation
-            elif re.match(
-                r"[+-]?([0-9]*[.])?[0-9]+",
-                value,
-            ):
+            elif re.match(r"[+-]?([0-9]*[.])?[0-9]+", value):
                 # Match a float with regex
                 temp.append(float(value))
             else:
