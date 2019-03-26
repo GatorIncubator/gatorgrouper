6ea4502af88c6504964a58b10c089f75f4a1495c
Aubrey Collins <collinsa@allegheny.edu>
2019-03-26 17:38:15 -0400

    Change variable names to clarify

--- gatorgrouper_cli.py
+++ gatorgrouper_cli.py
@@ -23,13 +23,13 @@ if __name__ == "__main__":  # pragma: no cover
         preference = None
     else:
         # Filter out empty names and students without preferences from the preferences CSV
-        preference = [
+        preference_list = [
             [entry for entry in row if entry != ""]
             for row in read_student_file.read_csv_data(GG_ARGUMENTS.preferences)
             if len(row) > 1
         ]
         # Associate student with set of preferred students
-        preference = {row[0]: set(row[1:]) for row in preference}
+        preference = {row[0]: set(row[1:]) for row in preference_list}
     check_if_arguments_valid = parse_arguments.check_valid(GG_ARGUMENTS, input_list)
     if check_if_arguments_valid is False:
         print("Incorrect command-line arguments.")
